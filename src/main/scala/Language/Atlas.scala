fe516b04491f3d19c681f7695cea953c75521961
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-07-23 12:45:55 +0200

    scala-prototype: initial commit of Atlas
    The implementation of Atlas is bare-bone. This commit is mainly
    intended as an example of a concrete type argument that parametrizes
    lambda terms: Language.Atlas.Constant.
    
    Old-commit-hash: 058d1bf7b12e05dbae101f6317ab556a717bd162

---
+++ src/main/scala/Language/Atlas.scala
@@ -0,0 +1,21 @@
+/**
+ * A description of the calculus Atlas
+ *
+ * Agda original:
+ * https://github.com/ps-mr/ilc/blob/9aafd0c2835ff027b57e44ed2930f4f57147e0de/agda/Syntax/Language/Atlas.agda
+ */
+
+package Language
+
+object Atlas extends Calculus {
+  import Syntax._
+
+  sealed trait Constant
+  case object True  extends Constant
+  case object False extends Constant
+
+  def deriveConst(c: Constant): Term = c match {
+  case True  => Const(False)
+  case False => Const(False)
+  }
+}
---
+++ src/main/scala/Language/Calculus.scala
@@ -0,0 +1,47 @@
+/**
+ * Features useful to all calculi
+ *
+ * - Specialize the parametric lambda calculus to the calculus's
+ *   set of constants
+ */
+
+package Language
+
+import Syntax.Lambda
+
+trait Calculus {
+
+  // SUBCLASS OBLIGATIONS
+
+  type Constant
+  def deriveConst(c: Constant): Syntax.Term
+
+  // SUBCLASS RIGHTS
+
+  // specialize the abstract syntax of lambda terms and their
+  // visitorsto the current calculus
+  val Syntax = Lambda.instantiate[Constant]
+
+  // import Syntax._
+  //
+  // Cai 23.07.13 [
+  //
+  //   We want all names in Syntax to be available to
+  //   subclasses. This import statement is useless, because
+  //   subclasses do not inherit imported names. I see 2 methods
+  //   to deal with the situation.
+  //
+  //   1. Write `import Syntax._` in the calculus Atlas, and write
+  //
+  //          import Language.Atlas._
+  //          import Language.Atlas.Syntax._
+  //
+  //      if one wants to bring everything pertaining to Atlas in
+  //      scope.
+  //
+  //   2. Duplicate interface declaration of Syntax here.
+  //  
+  //   Method 1 seems the lesser evil. Is there another way?
+  // ]
+
+}

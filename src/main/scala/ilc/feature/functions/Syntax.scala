284a12b38026fd14a2462a64c54baf7b1a6c9536
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-07-31 11:24:48 +0200

    feature.functions: add syntactic sugars for application and abstraction
    Write
    
        f(x)(y)(z)
    
    for
    
        App(App(App(f, x), y), z)
    
    and write
    
        Lambda("x", "y", "z") ->: t
    
    for
    
        Abs("x", Abs("y", Abs("z", t)))
    
    Old-commit-hash: f1af3632390a187af178e0d8d24e235d90bf08ab

--- src/main/scala/ilc/feature/functions/Syntax.scala
+++ src/main/scala/ilc/feature/functions/Syntax.scala
@@ -16,7 +16,42 @@ trait Syntax {
 
   // SYNTAX
 
-  sealed abstract trait Term
+  sealed abstract trait Term {
+    // easy way to build up nested applications
+    def apply(t: Term): App = App(this, t)
+
+    // easy way to build up nested abstractions
+    def ->:(name: String): Abs = Abs(name, this)
+    def ->:(variable: Var): Abs = variable.name ->: this
+    def ->:(parameterList: ParameterList): Term = parameterList match {
+      case EmptyParameterList =>
+        this
+      case NameBinding(name, otherBindings) =>
+        name ->: otherBindings ->: this
+    }
+  }
+
+  // helpers to syntactic sugar of abstraction
+  // basically a list of strings. unfortunately List[String]
+  // is a sealed class and can't be inherited.
+  sealed trait ParameterList
+  case object EmptyParameterList extends ParameterList
+  case class NameBinding(name: String,
+                         otherBindings: ParameterList)
+  extends ParameterList
+  object Lambda {
+    // unsafe argument type signature Any* is needed to work around
+    // scala's runtime type erasure
+    def apply(stuff: Any*): ParameterList =
+      if (stuff.isEmpty)
+        EmptyParameterList
+      else
+        NameBinding(stuff.head match {
+                      case name: String  => name
+                      case variable: Var => variable.name
+                    },
+                    apply(stuff.tail: _*))
+  }
 
   case class Var(name: String) extends Term
   case class App(operator: Term, operand: Term) extends Term

c6a68e04fea21437cb0c1e432b3f40a0c0677f1a
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-08-01 19:11:32 +0200

    DerivationSuite: purge Atlas.Type
    Old-commit-hash: db5467b2042ebe259a6df0196c3c479117434109

--- src/test/scala/ilc/language/atlas/DerivationSuite.scala
+++ src/test/scala/ilc/language/atlas/DerivationSuite.scala
@@ -12,27 +12,6 @@ import ilc.language.Atlas._
 
 class DerivationSuite extends FunSuite {
 
-  // SHORTHANDS
-
-  // TODO: remove me after the type system is removed
-  def neutral(dontcare: Type): Value = Value.Neutral
-
-  val emptyMap =
-    immutable.Map.empty[Value, Value].
-      withDefaultValue(Value.Neutral)
-
-  def lookup(key: Term, m: Term) =
-    eval(Lookup(Number, Number)(key)(m))
-
-  def foldNumMap(f: Term, z: Term, m: Term): Any =
-    eval(fold(Number, Number, f, z, m))
-
-  def diff(tau: Type, s: Term, t: Term): Term =
-    diffTerm(tau)(s)(t)
-
-  def apply(tau: Type, dt: Term, t: Term): Term =
-    applyTerm(tau)(dt)(t)
-
   // USABLE TERM CONSTRUCTORS
 
   // λf. λx. f x
@@ -45,43 +24,36 @@ class DerivationSuite extends FunSuite {
   val idFun = "x" ->: "x"
 
   def sum(t: Term): Term =
-    fold(Number, Number, constFun(Plus), 0, t)
+    Fold(constFun(Plus))(0)(t)
 
-  val negMap1234: Term =
-    mapLit(Number, Number, 1 -> -1, 2 -> -2, 3 -> -3, 4 -> -4)
+  val negMap1234: Term = Map(1 -> -1, 2 -> -2, 3 -> -3, 4 -> -4)
 
-  val negMap1256: Term =
-    mapLit(Number, Number, 1 -> -1, 2 -> -2, 5 -> -5, 6 -> -6)
+  val negMap1256: Term = Map(1 -> -1, 2 -> -2, 5 -> -5, 6 -> -6)
 
-  val idMap2367: Term = mapLit(Number, Number,
-    2 -> 2, 3 -> 3, 6 -> 6, 7 -> 7)
+  val idMap2367: Term = Map(2 -> 2, 3 -> 3, 6 -> 6, 7 -> 7)
 
-  val map1368: Term = mapLit(Number, Number,
-    1 -> 10, 3 -> 3, 6 -> 60, 8 -> 80)
+  val map1368: Term = Map(1 -> 10, 3 -> 3, 6 -> 60, 8 -> 80)
 
   val oddityMap1234: Term =
-    mapLit(Number, Bool, 1 -> True, 2 -> False, 3 -> True, 4 -> False)
+    Map(1 -> True, 2 -> False, 3 -> True, 4 -> False)
 
   val primeMap10: Term =
-    mapLit(Number, Bool,
-      2 -> True,  3 -> True, 4 -> False, 5 -> True,
-      6 -> False, 7 -> True, 8 -> False, 9 -> False, 10 -> False)
+    Map(2 -> True, 3 -> True, 4 -> False,  5 -> True, 6 -> False,
+        7 -> True, 8 -> False, 9 -> False, 10 -> False)
+
+  val nilTerm = Empty
 
   // TEST CANDIDATES
 
-  val typesAndTerms = List.apply[(Type, Term)](
-    Bool -> True,
-    Bool -> False,
-    Bool -> Lookup(Number, Bool)(2)(oddityMap1234),
-    Number -> 1984,
-    Number -> sum(negMap1256),
-    Map(Number, Number) -> negMap1234,
-    Map(Number, Bool) -> primeMap10,
-    Map(Map(Number, Number), Map(Number, Bool)) ->
-      mapLit(Map(Number, Number), Map(Number, Bool),
-             negMap1234 -> oddityMap1234,
-             negMap1256 -> primeMap10)
-  )
+  val terms = List.apply[Term](
+    True,
+    False,
+    Lookup(2)(oddityMap1234),
+    1984,
+    sum(negMap1256),
+    negMap1234,
+    primeMap10,
+    Map(negMap1234 -> oddityMap1234, negMap1256 -> primeMap10))
 
   val intsAndInts = {
     val lhs = Array(914, 649, 869, 432, 795, 761, 1, 3, 5)
@@ -109,27 +81,22 @@ class DerivationSuite extends FunSuite {
   // TESTING ENCODINGS
 
   test("pair encoding preserves information") {
-    val typeList: List[(Type, Type)] =
-      List((Number, Bool),
-           (Bool, Map(Number, Bool)),
-           (Map(Number, Number), Map(Number, Bool)))
     val termList: List[(Term, Term)] =
       List((0, False),
-           (Lookup(Number, Bool)(2)(oddityMap1234), primeMap10),
-           (negMap1234, Lookup(Number, Bool)(4)(primeMap10)),
+           (Lookup(2)(oddityMap1234), primeMap10),
+           (negMap1234, Lookup(4)(primeMap10)),
            (negMap1256, primeMap10))
     val fst = "x" ->: "y" ->: "x"
     val snd = "x" ->: "y" ->: "y"
-    def mkProj(proj: Term): (Type, Type, Term, Term) => Any =
-      (sType, tType, s, t) =>
-        eval(uncurry(sType, tType, proj, pair(sType, tType, s, t)))
+    def mkProj(proj: Term): (Term, Term) => Value =
+      (s, t) => eval(uncurry(proj, pair(s, t)))
     val proj1 = mkProj(fst)
     val proj2 = mkProj(snd)
-    (typeList, termList).zipped.foreach {
-      (types, terms) => {
-        val ((tau1, tau2), (t1, t2)) = (types, terms)
-        assert(proj1(tau1, tau2, t1, t2) === eval(t1))
-        assert(proj2(tau1, tau2, t1, t2) === eval(t2))
+    termList.foreach {
+      terms => {
+        val (t1, t2) = terms
+        assert(proj1(t1, t2) === eval(t1))
+        assert(proj2(t1, t2) === eval(t2))
       }
     }
   }
@@ -137,11 +104,8 @@ class DerivationSuite extends FunSuite {
   test("zip4 traverses the union of all maps") {
     val plus4 = Lambda("k", "a", "b", "c", "d") ->:
       Plus("a", "b", "c", "d")
-    def ezmap(i: Int, j: Int) =
-      mapLit(Number, Number, i -> j)
-    def t = zip4(Number, Number, Number, Number, Number, Number,
-      plus4,
-      ezmap(1, 2), ezmap(3, 4), ezmap(5, 6), ezmap(7, 8))
+    def t = zip4(plus4,
+                 Map(1 -> 2), Map(3 -> 4), Map(5 -> 6), Map(7 -> 8))
     assertMap(t, 1 -> 2, 3 -> 4, 5 -> 6, 7 -> 8)
   }
 
@@ -166,21 +130,13 @@ class DerivationSuite extends FunSuite {
   }
 
   test("Empty is empty map") {
-    // assertMap(t: Term, assoc: (Value, Value)*)
-    // when no other argument is given,
-    // assertMap(t) tests if t evaluates to the empty map.
-    assertMap(Empty(Bool, Bool))
-    // test that type parameters to Emtpy don't influence anything
-    assertMap(Empty(Map(Bool, Map(Bool, Bool)),
-                    Map(Map(Bool, Bool), Bool)))
+    assertMap(Empty, Nil: _*)
   }
 
   test("neutral elements are false, 0, emptyMap") {
-    assert(neutral(Bool).toBool === false)
-    assert(neutral(Number).toInt === 0)
-    assert(neutral(Map(Number, Bool)).toMap === emptyMap)
-    assert(neutral(Map(Map(Bool, Map(Bool, Bool)),
-      Map(Map(Bool, Bool), Bool))).toMap === emptyMap)
+    assert(Value.Neutral.toBool === false)
+    assert(Value.Neutral.toInt === 0)
+    assert(Value.Neutral.toMap === ValueMap())
   }
 
   test("updating with nonexistent key is insertion") {
@@ -188,33 +144,31 @@ class DerivationSuite extends FunSuite {
   }
 
   test("updating with neutral element is deletion") {
-    assertMap(updatesFrom(Number, Number, negMap1234,
-                          1 -> 0, 3 -> 0, 5 -> 0),
+    assertMap(updatesFrom(negMap1234, 1 -> 0, 3 -> 0, 5 -> 0),
               2 -> -2, 4 -> -4)
-    assertMap(updatesFrom(Number, Bool, oddityMap1234, 1 -> False),
+    assertMap(updatesFrom(oddityMap1234, 1 -> False),
               3 -> true)
   }
 
   test("updating with existing key changes the value") {
-    assertMap(updatesFrom(Number, Number, negMap1234, 2 -> 99, 3 -> 75),
+    assertMap(updatesFrom(negMap1234, 2 -> 99, 3 -> 75),
               1 -> -1, 2 -> 99, 3 -> 75, 4 -> -4)
   }
 
   test("looking up an existing key returns the associated value") {
     Range.inclusive(1, 4) foreach { i =>
-      assert(lookup(i, negMap1234).toInt === -i)
+      assert(eval(Lookup(i)(negMap1234)).toInt === -i)
     }
   }
 
   test("looking up a nonexistent key returns the neutral element") {
     Range.inclusive(5, 9) foreach { i =>
-      assert(lookup(i, negMap1234) === neutral(Number))
+      assert(eval(Lookup(i)(negMap1234)) === Value.Neutral)
     }
   }
 
   test("zipping two maps traverses the union of their keys") {
-    assertMap(zip(Number, Number, Number, Number,
-                  constFun(Plus), negMap1234, negMap1256),
+    assertMap(Zip(constFun(Plus))(negMap1234)(negMap1256),
               1 -> (- 1 - 1), 2 -> (- 2 - 2),
               3 -> -3, 4 -> -4, 5 -> -5, 6 -> -6)
   }
@@ -227,55 +181,48 @@ class DerivationSuite extends FunSuite {
   // TESTING DIFF, APPLY, NIL
 
   test("nil-terms denote nil changes") {
-    def applyNil(tau: Type, t: Term): Value =
-      eval(apply(tau, nilTerm(tau), t))
+    def applyNil(t: Term): Value = eval(Apply(nilTerm)(t))
 
-    typesAndTerms.foreach { p =>
-      assert(applyNil(p._1, p._2) === eval(p._2))
+    terms.foreach { t =>
+      assert(applyNil(t) === eval(t))
     }
   }
 
   test("difference with self is the nil change") {
-    typesAndTerms.foreach { p =>
-      assert(eval(diff(p._1, p._2, p._2)) === eval(nilTerm(p._1)))
+    terms.foreach { t =>
+      assert(eval(Diff(t)(t)) === Value.Neutral)
     }
   }
 
   test("[s ⊕ (t ⊝ s) == t] holds for Booleans, numbers and maps") {
-    def applyDiff(tau: Type, s: Term, t: Term): Any =
-      eval(apply(tau, diff(tau, t, s), s))
-    List.apply[(Type, Term, Term)](
-      (Bool, False, False),
-      (Bool, False, True ),
-      (Bool, True , False),
-      (Bool, True , True ),
-      (Number, 392, -1522),
-      (Number, sum(negMap1234), sum(negMap1256)),
-      (Map(Number, Number), negMap1234, negMap1256),
-      (Map(Number, Bool), primeMap10, oddityMap1234),
-      (Map(Map(Number, Number), Map(Number, Bool)),
-        mapLit(Map(Number, Number), Map(Number, Bool),
-               negMap1234 -> oddityMap1234,
-               negMap1256 -> primeMap10,
-               mapLit(Number, Number, 99 -> 217) ->
-                 mapLit(Number, Bool, 2012 -> True)),
-        mapLit(Map(Number, Number), Map(Number, Bool),
-               negMap1234 -> primeMap10,
-               negMap1256 -> primeMap10,
-               Empty(Number, Number) -> primeMap10))
-    ).foreach { trio =>
-      assert(applyDiff(trio._1, trio._2, trio._3) === eval(trio._3))
+    def applyDiff(s: Term, t: Term): Value = eval(Apply(Diff(t)(s))(s))
+    List.apply[(Term, Term)](
+      (False, False),
+      (False, True ),
+      (True , False),
+      (True , True ),
+      (392, -1522),
+      (sum(negMap1234), sum(negMap1256)),
+      (negMap1234, negMap1256),
+      (primeMap10, oddityMap1234),
+      (Map(negMap1234 -> oddityMap1234,
+           negMap1256 -> primeMap10,
+           Map(99 -> 217) -> Map(2012 -> True)),
+       Map(negMap1234 -> primeMap10,
+           negMap1256 -> primeMap10,
+           Empty -> primeMap10))
+    ).foreach { duo =>
+      assert(applyDiff(duo._1, duo._2) === eval(duo._2))
     }
   }
 
   // TESTING DERIVATIVES OF CONSTANTS
 
   test("the derivative of base-type constants is the nil change") {
-    assert(eval(derive(True)) === eval(nilTerm(Bool)))
-    assert(eval(derive(False)) === eval(nilTerm(Bool)))
-    assert(eval(derive(Num(12341))) === eval(nilTerm(Number)))
-    assert(eval(derive(Empty(Bool, Number))) ===
-           eval(nilTerm(Map(Bool, Number))))
+    assert(eval(derive(True)) === Value.Neutral)
+    assert(eval(derive(False)) === Value.Neutral)
+    assert(eval(derive(Num(12341))) === Value.Neutral)
+    assert(eval(derive(Empty)) === Value.Neutral)
   }
 
 
@@ -283,30 +230,29 @@ class DerivationSuite extends FunSuite {
 
     def isOdd(i: Int): Term = if (i % 2 == 1) True else False
     Range.inclusive(0, 16).foreach { i =>
-      assertCorrect(Bool, Xor,
-        (Bool, isOdd(i / 8), isOdd(i / 4)) ::
-        (Bool, isOdd(i / 2), isOdd(i)) :: Nil)
+      assertCorrect(Xor,
+        (isOdd(i / 8), isOdd(i / 4)) ::
+        (isOdd(i / 2), isOdd(i)) :: Nil)
     }
 
     (intsAndInts, intsAndInts.reverse).zipped.foreach { (p1, p2) =>
       val ((x, xNew), (yNew, y)) = (p1, p2)
-      assertCorrect(Number, Plus,
-        List((Number, x, xNew), (Number, y, yNew)))
+      assertCorrect(Plus, List((x, xNew), (y, yNew)))
     }
   }
 
   test("the derivative of Negate is correct") {
     intsAndInts.foreach { p =>
-      assertCorrect(Number, Negate, List((Number, p._1, p._2)))
+      assertCorrect(Negate, List(p))
     }
   }
 
   test("the derivative of Update is correct") {
     (intsAndInts, intsAndInts.reverse).zipped.foreach { (p, q) =>
-      assertCorrect(Map(Number, Number), Update(Number, Number),
-        List((Number, p._1, p._2), // old/new keys
-             (Number, q._1, q._2), // old/new values
-             (Map(Number, Number), negMap1234, negMap1256)))
+      assertCorrect(Update,
+        List(p, // old/new keys
+             q, // old/new values
+             (negMap1234, negMap1256)))
     }
   }
 
@@ -321,22 +267,20 @@ class DerivationSuite extends FunSuite {
     (List(1,1,1,1,3,3,3,3,5,5,5,5,7,7,7,7),
      List(1,3,5,7,1,3,5,7,1,3,5,7,1,3,5,7)).zipped.foreach {
       (i, j) =>
-        assertCorrect(Number, Lookup(Number, Number),
-          List((Number, i, j), // old/new keys
-               (Map(Number, Number), negMap1256, negMap1234)))
+        assertCorrect(Lookup,
+          List((i, j), // old/new keys
+               (negMap1256, negMap1234)))
     }
   }
 
   test("the derivative of Zip is correct") {
-    val m = Map(Number, Number)
-    val n = Number
     // λx. zip (λ_. λy. λz. x + y + z)
     val t = "x" ->:
-      Zip(n, n, n, n)(Lambda("_", "y", "z") ->: Plus("x", "y", "z"))
-    assertCorrect(m, t,
-      List((n, 100, 1000),
-           (m, negMap1234, negMap1256),
-           (m, idMap2367, map1368)))
+      Zip(Lambda("_", "y", "z") ->: Plus("x", "y", "z"))
+    assertCorrect(t,
+      List((100, 1000),
+           (negMap1234, negMap1256),
+           (idMap2367, map1368)))
     // this is a good test case: result change contains all of
     // insertion, deletion and update.
   }
@@ -448,15 +392,14 @@ class DerivationSuite extends FunSuite {
   //
   //     f x ⊕ derive(f) x (y ⊝ x) == f y
   //
-  def assertCorrect(tau: Type,
-                    f: Term,
-                    input: List[(Type, Term, Term)]) {
+  def assertCorrect(f: Term,
+                    input: List[(Term, Term)]) {
     val reversedInput = input.reverse
     val theOld    = assembleOld(f, reversedInput)
     val theNew    = assembleNew(f, reversedInput)
     val theChange = assembleChange(derive(f), reversedInput)
     try {
-      assert(eval(apply(tau, theChange, theOld)) === eval(theNew))
+      assert(eval(Apply(theChange)(theOld)) === eval(theNew))
     } catch { case err: TestFailedException =>
       val msg = "wrong derivative:" ++
         "\n     old = " ++ eval(theOld).toString ++
@@ -470,26 +413,26 @@ class DerivationSuite extends FunSuite {
   }
 
   def assembleChange(dt: Term,
-                     reversedInput: List[(Type, Term, Term)]): Term =
+                     reversedInput: List[(Term, Term)]): Term =
     reversedInput match {
       case Nil => dt
-      case (tau, z, zNew) :: theRest => {
-        val dz = diff(tau, zNew, z)
+      case (z, zNew) :: theRest => {
+        val dz = Diff(zNew)(z)
         assembleChange(dt, theRest)(z)(dz)
       }
     }
 
-  val assembleOld = assembleAccordingTo(_._2)
-  val assembleNew = assembleAccordingTo(_._3)
+  val assembleOld = assembleAccordingTo(_._1)
+  val assembleNew = assembleAccordingTo(_._2)
 
-  def assembleAccordingTo(chooser: ((Type, Term, Term)) => Term):
-        (Term, List[(Type, Term, Term)]) => Term = {
+  def assembleAccordingTo(chooser: ((Term, Term)) => Term):
+        (Term, List[(Term, Term)]) => Term = {
     def assemble(t: Term,
-                 reversedInput: List[(Type, Term, Term)]): Term =
+                 reversedInput: List[(Term, Term)]): Term =
       reversedInput match {
         case Nil => t
-        case triple :: theRest =>
-          assemble(t, theRest)(chooser(triple))
+        case duo :: theRest =>
+          assemble(t, theRest)(chooser(duo))
       }
     assemble
   }

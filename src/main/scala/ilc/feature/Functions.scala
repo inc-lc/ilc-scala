8d40b36e36fbf489b37115dce68b2f97d054f97b
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-08-01 10:24:13 +0200

    feature.Functions: indent it like scala standard library.
    Old-commit-hash: 8273c99d9711842e235bcb1566ca09519f4289da

--- src/main/scala/ilc/feature/Functions.scala
+++ src/main/scala/ilc/feature/Functions.scala
@@ -5,15 +5,15 @@ package feature
  * First-class functions.
  */
 trait Functions
-  extends functions.Syntax
-  with functions.Pretty
-  with functions.Derivation
-  with functions.UniqueNames
-  with functions.Evaluation
+extends functions.Syntax
+   with functions.Pretty
+   with functions.Derivation
+   with functions.UniqueNames
+   with functions.Evaluation
 
 object Functions extends Functions {
   type Constant = Nothing
 
-  def deriveConst(constant : Constant) = ???
+  def deriveConst(constant: Constant) = ???
   def evalConst(constant: Constant) = ???
 }

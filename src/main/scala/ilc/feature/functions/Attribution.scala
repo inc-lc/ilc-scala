8e70c914d68abf894f664c295506acc9fe1f3cb1
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-08-02 15:58:48 +0200

    feature.functions: introduce 1-hole-contexts and attributes
    Old-commit-hash: 1b93f43891c0972dc106e226230253060ce68d40

---
+++ src/main/scala/ilc/feature/functions/Attribution.scala
@@ -0,0 +1,44 @@
+package ilc
+package feature.functions
+
+/**
+ * Interface for attributing abstract syntax trees
+ */
+
+import scala.language.implicitConversions
+import scala.collection.mutable
+
+trait Attribution extends Context { self: Syntax =>
+
+  abstract class Attribute[T](root: Term) {
+
+    def apply(s: Subterm): T = {
+      assert(root == s.root)
+      store(s)
+    }
+
+    // alias of this.apply for the benefit of subclasses
+    protected def lookup(s: Subterm): T = apply(s)
+
+    def update(s: Subterm, value: T) {
+      assert(root == s.root)
+      store.update(s, value)
+    }
+
+    private[this] var store: mutable.Map[Subterm, T] =
+      mutable.Map.empty
+  }
+
+  abstract class SyntheticAttribute[T](root: Term)
+  extends Attribute[T](root) {
+
+    // ensures: attributes of all subterms are available in body.
+    def synthesize(s: Subterm): T
+
+    init(Subterm.refl(root))
+    private[this] def init(s: Subterm): Unit = {
+      s.eachChild(init)
+      update(s, synthesize(s))
+    }
+  }
+}

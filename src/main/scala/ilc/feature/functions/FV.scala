8fe05cf206abdaa1fe009dd027f44e6c996197ff
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-08-02 15:59:53 +0200

    feature.functions: rephrase free-variables as a synthetic attribute
    Old-commit-hash: 0cae0880d3ff2a86e58a4a8ec1a2baa08ff01af3

--- src/main/scala/ilc/feature/functions/FV.scala
+++ src/main/scala/ilc/feature/functions/FV.scala
@@ -5,19 +5,20 @@ package feature.functions
  * Free variables
  */
 
-trait FV extends Syntax {
-  def FV(t: Term): Set[String] = t match {
-    case Const(constant) =>
-      Set.empty
+trait FV extends Attribution { self: Syntax =>
 
-    case Var(name) =>
-      Set(name)
+  def FV(t: Term): Set[String] = FV_attr(t)(Subterm.refl(t))
 
-    case App(operator, operand) =>
-      FV(operator) ++ FV(operand)
-
-    case Abs(name, body) =>
-      FV(body) - name
+  case class FV_attr(root: Term)
+  extends SyntheticAttribute[Set[String]](root) {
+    def synthesize(s: Subterm): Set[String] = s.term match {
+      case Const(_) =>
+        Set.empty
+      case Var(name) =>
+        Set(name)
+      case App(_, _) | Abs(_, _) =>
+        s.down.map(lookup).fold(Set.empty)(_ ++ _)
+    }
   }
 }
 

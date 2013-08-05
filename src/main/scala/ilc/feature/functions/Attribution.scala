ef3384c8565bd8215a7713e8707448307c72eff1
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-08-05 16:28:52 +0200

    feature.functions.Attribution: rewrite interface (#100)
    Old-commit-hash: 168ca21f595bdd0ad56f6b056a3d305770f39115

--- src/main/scala/ilc/feature/functions/Attribution.scala
+++ src/main/scala/ilc/feature/functions/Attribution.scala
@@ -12,56 +12,80 @@ trait Attribution extends Context { self: Syntax =>
 
   abstract class Attribute[T](root: Term) {
 
-    def apply(s: Subterm): T = {
-      require(root == s.root)
-      store(s)
-    }
+    def lookup(s: Subterm): T
+    def update(s: Subterm, value: T): Unit
 
-    // alias of this.apply for the benefit of subclasses
-    protected def lookup(s: Subterm): T = apply(s)
-
-    def update(s: Subterm, value: T) {
-      require(root == s.root)
-      store.update(s, value)
-    }
-
-    def isDefined(s: Subterm): Boolean =
-      store.contains(s)
-
-    private[this] var store: mutable.Map[Subterm, T] =
-      mutable.Map.empty
+    def apply(s: Subterm): T = lookup(s)
   }
 
+  /**
+   * The abstract class SynthesizedAttribute makes it convenient
+   * to write synthesized attributes. While it is not enforced
+   * that subclasses has to be synthesized attributes, a non-
+   * synthesized attribute inheriting from it will almost always
+   * be computed wrong.
+   */
   abstract class SynthesizedAttribute[T](root: Term)
   extends Attribute[T](root) {
 
-    def synthesize(s: Subterm): T
+    def synthesize(s: Subterm, childAttr: List[T]): T
+
+    def lookup(s: Subterm): T = lookup(s.term)
+    def lookup(t: Term): T = store(t)
+    def update(s: Subterm, value: T): Unit = update(s.term, value)
+    def update(t: Term, value: T): Unit = store.update(t, value)
+
+    private[this] var store: mutable.Map[Term, T] =
+      mutable.Map.empty
 
     init(Subterm.refl(root))
     private[this] def init(s: Subterm): Unit = {
       s.eachChild(init)
-      // checks that the attribute is defined for all
-      // children after initialization
-      assert(s.children.map(isDefined).fold(true)(_ && _))
-      update(s, synthesize(s))
+      update(s,
+        synthesize(s,
+          s.children.map(lookup)))
     }
   }
 
+  /**
+   * The abstract class InheritedAttribute makes it convenient
+   * to write inherited attributes. It is not enforced that
+   * every subclass has to be an inherited attribute.
+   */
   abstract class InheritedAttribute[T](root: Term)
   extends Attribute[T](root) {
 
     val rootAttr: T
-    def inherit(s: Subterm, parentAttr: T, childNumber: Int): T
+
+    /**
+     * Specification of an inherited attribute
+     *
+     * @param s
+     *   subterm whose attribute is to be calculated
+     * @param childNumber
+     *   seniority of {@code s} w.r.t. its parent:
+     *   in {@code App(p, q)}, the {@code childNumber}
+     *   of {@code p} is 0 and the {@code childNumber}
+     *   of {@code q} is 1.
+     * @param parentAttr
+     *   the attribute value of the parent of {@code s}
+     */
+    def inherit(s: Subterm, childNumber: Int, parentAttr: T): T
+
+    def lookup(s: Subterm): T = store(s)
+    def update(s: Subterm, value: T): Unit = store.update(s, value)
+
+    private[this] var store: mutable.Map[Subterm, T] =
+      mutable.Map.empty
 
     update(Subterm.refl(root), rootAttr)
     init(Subterm.refl(root))
     
     private[this] def init(s: Subterm): Unit = {
-      assert(isDefined(s))
       val s_attr = lookup(s)
       s.children.zipWithIndex.foreach {
         case (child, childNumber) =>
-          update(child, inherit(child, s_attr, childNumber))
+          update(child, inherit(child, childNumber, s_attr))
       }
     }
   }

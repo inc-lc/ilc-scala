e1b1610222ba96217459c60806b766942def3e9d
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-08-05 16:30:15 +0200

    feature.functions.FV: use improved Attribution interface (fixes #100)
    Old-commit-hash: c06fd788d29b2084f67218f9ba88d0d4cdaed67d

--- src/main/scala/ilc/feature/functions/FV.scala
+++ src/main/scala/ilc/feature/functions/FV.scala
@@ -11,13 +11,13 @@ trait FV extends Attribution { self: Syntax =>
 
   case class FV_attr(root: Term)
   extends SynthesizedAttribute[Set[String]](root) {
-    def synthesize(s: Subterm): Set[String] = s.term match {
-      case Const(_) =>
-        Set.empty
-      case Var(name) =>
-        Set(name)
-      case App(_, _) | Abs(_, _) =>
-        s.children.map(lookup).fold(Set.empty)(_ ++ _)
-    }
+    def synthesize(s: Subterm,
+                   childAttr: List[Set[String]]): Set[String] =
+      s.term match {
+        case Var(name) =>
+          Set(name)
+        case _ =>
+          childAttr.fold(Set.empty)(_ ++ _)
+      }
   }
 }

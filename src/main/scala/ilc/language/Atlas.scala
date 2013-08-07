6ed02b828def74b5a60efa0a1b2c337515fe592b
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-08-07 16:39:30 +0200

    enable pretty printing for Atlas and Bacchus
    Old-commit-hash: a228b6a8775f9824abaccc670f94285fe0523610

--- src/main/scala/ilc/language/Atlas.scala
+++ src/main/scala/ilc/language/Atlas.scala
@@ -5,9 +5,11 @@ package language
  * The calculus Atlas in one convenient object
  */
 
+import feature.functions
 import atlas._
 
 object Atlas
 extends Syntax
    with Evaluation
    with Derivation
+   with functions.Pretty
--- src/main/scala/ilc/language/Bacchus.scala
+++ src/main/scala/ilc/language/Bacchus.scala
@@ -5,9 +5,11 @@ package language
  * The calculus Bacchus in one convenient object
  */
 
+import feature.functions
 import bacchus._
 
 object Bacchus
 extends Syntax
    with Evaluation
    with Derivation
+   with functions.Pretty

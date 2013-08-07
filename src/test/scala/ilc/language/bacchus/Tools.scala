913e94d007471e8f67b094fc1aff54de62a31a07
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-08-07 16:42:32 +0200

    language.Bacchus.Tools: add derivation test with fine-grained differences
    A fine-grained difference is not a replacement value. For maps, it
    stores keys to be deleted, key-value pairs to be inserted, and
    values to be updated. For sums, the difference between Left(x)
    and Left(y) is stored as a tagged difference between x and y,
    and so is the difference between Right(x) and Right(y).
    
    The tool `assertCorrectTwice` tests the correctness of a derivative
    both on the result of diff-term and on a fine-grained difference.
    
    Old-commit-hash: 8cd289be3312a3c569b2b54f2c0c58fa1f8a03da

--- src/test/scala/ilc/language/bacchus/Tools.scala
+++ src/test/scala/ilc/language/bacchus/Tools.scala
@@ -30,4 +30,55 @@ extends DerivationTools { self: FunSuite =>
   def assertMapVal(t: ValueMap, assoc: (Value, Value)*) {
     assert(t === immutable.Map(assoc: _*))
   }
+
+  // DERIVATION TEST WITH FINE-GRAINED CHANGES
+  def assertCorrectTwice(f: Term, input: List[(Term, Term)]) {
+    assertCorrect(f, input)
+    assertCorrect(f, input, fineGrainedDiff)
+  }
+
+  def fineGrainedDiff(x: Term, y: Term): Term =
+    (eval(x), eval(y)) match {
+      case (vx: BacchusValue, vy: BacchusValue) =>
+        reify(bacchusDiff(vx, vy))
+      case _ =>
+        Diff(x)(y)
+    }
+
+  def bacchusDiff(x: BacchusValue,
+                  y: BacchusValue): BacchusValue = (x, y) match {
+    case (Value.Left(lx), Value.Left(ly)) =>
+      Value.Left(Value.Left(bacchusDiff(lx, ly)))
+    case (Value.Right(lx), Value.Right(ly)) =>
+      Value.Right(Value.Right(bacchusDiff(lx, ly)))
+    case (Value.Map(m1), Value.Map(m2)) => {
+      val toDelete = m2.keySet -- m1.keySet
+      val toInsert = m1 -- m2.keySet
+      val toUpdate = (m1.keySet & m2.keySet) filter {
+        key => m1(key) != m2(key)
+      }
+      Value.Left(
+        toInsert.map {
+          case (key, value) => key -> Value.Left(Value.Right(value))
+        } ++ toDelete.map {
+          key => key -> Value.Left(Value.Left(Value.Individualist))
+        } ++ toUpdate.map {
+          key => key -> Value.Right(bacchusDiff(m1(key), m2(key)))
+        })
+    }
+    case _ => Value.diff(x, y) match {
+      case bv: BacchusValue => bv
+      case _ => sys.error("the difference between " ++
+        "two Bacchus-values is not a Bacchus-value?!")
+    }
+  }
+
+  def reify(x: BacchusValue): Term = x match {
+    case Value.Individualist => Individualist
+    case Value.Nat(n) => Nat(n)
+    case Value.Map(m) => Map(m.map({case (key, value) =>
+      reify(key) -> reify(value)}).toList: _*)
+    case Value.Left(x) => Left(reify(x))
+    case Value.Right(y) => Right(reify(y))
+  }
 }

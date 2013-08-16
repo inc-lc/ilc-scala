01c78ae9ae16f589c66ee5f65b38f73afa9b87b9
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-08-16 17:15:49 +0200

    ilc.feature.Functions: Separate calculus-independent features
    Old-commit-hash: c1fc3d6fde9f18dd6c255e80647de35fdd1bf207

--- src/main/scala/ilc/feature/Functions.scala
+++ src/main/scala/ilc/feature/Functions.scala
@@ -6,10 +6,11 @@ package feature
  */
 trait Functions
 extends functions.Syntax
-   with functions.Pretty
-   with functions.Derivation
-   with functions.UniqueNames
    with functions.Evaluation
+   with functions.Derivation
+   /* Calculus/feature-independent infrastructure below this line. */
+   with functions.Pretty
+   with functions.UniqueNames
    with functions.Attribution
 
 object Functions extends Functions {

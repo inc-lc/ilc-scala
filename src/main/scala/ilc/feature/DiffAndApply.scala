13f87d77d8be0fd18570d5cce027f877b7e927d7
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-08-20 01:20:43 +0200

    feature.DiffAndApply: Remove extra requirement
    Old-commit-hash: 6bd8d51e6e16bda7ba7dd93fd3060b7e1f06857b

--- src/main/scala/ilc/feature/DiffAndApply.scala
+++ src/main/scala/ilc/feature/DiffAndApply.scala
@@ -5,7 +5,7 @@ package feature
  * Commonality of calculi with diff/apply terms
  */
 
-trait DiffAndApply { self: functions.Syntax =>
+trait DiffAndApply { self: base.Syntax =>
   val diffTerm: Term
   val applyTerm: Term
 }

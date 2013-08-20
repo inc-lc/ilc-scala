inc-lc/ilc-scala

commit 42a511c2d6775bf7bfa08acdd8d0cd57d6e53d84

Don't catch and rethrow twice the same exception
Old-commit-hash: 2f8aab6029c1378d73877db12419642f3438f7e3

src/main/scala/ilc/feature/functions/Evaluation.scala
@@ -18,13 +18,13 @@ trait Evaluation { self: Syntax =>
 
   def eval(t: Term): Value = try {
     evalWithEnv(t, immutable.Map.empty)
-  } catch { case err: IllegalArgumentException =>
+  } catch { case err: InvalidTargetObjectTypeException =>
     throw new
       IllegalArgumentException(err.getMessage() ++
         "\n in the term\n    " ++ t.toString)
   }
 
-  def evalWithEnv(t: Term, env: Env): Value = try {
+  def evalWithEnv(t: Term, env: Env): Value =
     t match {
       case Abs(x, t) =>
         (arg: Value) => evalWithEnv(t, env.updated(x, arg))
@@ -35,11 +35,6 @@ trait Evaluation { self: Syntax =>
       case Const(c) =>
         evalConst(c)
     }
-  } catch { case err: InvalidTargetObjectTypeException =>
-    throw new
-      IllegalArgumentException(err.getMessage() ++
-        " when evaluating:\n    " ++ t.toString)
-  }
 
   trait Value {
     // "toFunction"

commit ac557eeeefe2dce6aade903c28da17c5a90727b0

Evaluation exceptions should record the failing subterm
Fix breakage introduced by 42a511c2d6775bf7bfa08acdd8d0cd57d6e53d84.
Here I do not reintroduce the original code, I'm just inspired by it
and reuse the same algorithm, but I try to clarify what's going on and
make it more structured.

A unit test for this is needed.

Old-commit-hash: e03f968d55412ea3d528f2eb8be9c27dfc5bb784

src/main/scala/ilc/feature/base/Evaluation.scala
@@ -12,6 +12,7 @@ trait Evaluation extends Syntax {
   def evalConst(c: Constant): Value = { die(c, "evalConst") }
 
   //Core of the evaluation function, to be extended by subclasses.
+  //Recursive calls should be done only through wrapEval.
   def coreEval(t: Term, env: Env): Value =
     t match {
       case Const(c) =>
@@ -22,6 +23,30 @@ trait Evaluation extends Syntax {
   // Subclass obligations end //
   //////////////////////////////
 
+  //Record the evaluated subterm
+  def wrapEval(t: Term, env: Env): Value =
+    try {
+      coreEval(t, env)
+    } catch { case ute @ UnexpectedTypeException(InnerTypeExceptionInfo(msg), _) =>
+      throw new UnexpectedTypeException(OuterTypeExceptionInfo(msg, t), ute)
+    }
+
+  // Data is not put directly in the exception, but in nested case classes,
+  // because exceptions seem to implement toString in a fixed way (which makes
+  // sense) - or they just don't use toString but getMessage in the output, more
+  // likely.
+  case class UnexpectedTypeException(data: TypeExceptionInfo, cause: Throwable = null)
+    extends Exception(data.toString, cause)
+
+  trait TypeExceptionInfo
+  case class InnerTypeExceptionInfo(message: String) extends TypeExceptionInfo
+  case class OuterTypeExceptionInfo(message: String, subterm: Term, term: Option[Term] = None) extends TypeExceptionInfo {
+    override def toString = {
+      val optTermStr = term map ("\n>> Term: " + _.toString) getOrElse ""
+      s"${message}\n>> Subterm: ${subterm}${optTermStr}"
+    }
+  }
+
   trait Value {
     // "toFunction"
     def apply(argument: Value): Value = die("apply", argument)
@@ -31,23 +56,18 @@ trait Evaluation extends Syntax {
 
   type Env = immutable.Map[String, Value]
 
-  class InvalidTargetObjectTypeException(message: String) extends Exception(message)
-
   def eval(t: Term): Value = try {
-    coreEval(t, immutable.Map.empty)
-  } catch { case err: InvalidTargetObjectTypeException =>
-    throw new
-      IllegalArgumentException(err.getMessage() ++
-        "\n in the term\n    " ++ t.toString)
+    wrapEval(t, immutable.Map.empty)
+  } catch { case UnexpectedTypeException(info: OuterTypeExceptionInfo, cause) =>
+    throw UnexpectedTypeException(info.copy(term = Some(t)), cause)
   }
 
-
   def die(value: Any, from: String, arg: Any = ""): Nothing =
-    throw new
-      InnerUnexpectedTypeException(value.toString ++
+    throw UnexpectedTypeException(
+      InnerTypeExceptionInfo(value.toString ++
         "." ++ from ++
         (if (arg.toString == "")
           ""
         else
-          "(" ++ arg.toString ++ ")"))
+          "(" ++ arg.toString ++ ")")))
 }

src/main/scala/ilc/feature/functions/Evaluation.scala
@@ -14,9 +14,9 @@ trait Evaluation extends base.Evaluation with Syntax {
   override def coreEval(t: Term, env: Env): Value =
     t match {
       case Abs(x, t) =>
-        (arg: Value) => coreEval(t, env.updated(x, arg))
+        (arg: Value) => wrapEval(t, env.updated(x, arg))
       case App(s, t) =>
-        coreEval(s, env)(coreEval(t, env))
+        wrapEval(s, env)(wrapEval(t, env))
       case Var(name) =>
         env(name) // NoSuchElementException = free var
       case _ =>
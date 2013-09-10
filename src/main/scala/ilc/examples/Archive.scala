c8dcf047446f539e3ef92356c1a0d37ab712e10d
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-10 19:04:37 +0200

    add example generation facility with sbt interop in mind
    Old-commit-hash: cf7fdc6f930e7694782fa251f40d331999270652

---
+++ src/main/scala/ilc/examples/Archive.scala
@@ -0,0 +1,45 @@
+package ilc
+package examples
+
+// the storage aspect of an examples generator
+trait Archive {
+  import scala.collection.mutable
+  val archive: mutable.Map[String, Example] = mutable.Map.empty
+
+  def get(exampleWithName: String): Example =
+    archive(exampleWithName)
+
+  abstract class Example {
+    val calculus: feature.base.ToScala
+    import calculus._
+    def program: Term
+    def derivative: Term
+
+    def toSource(name: String): Source = {
+      val objectName = name ++ "Binary"
+      val programCode = toScala(program)
+      val derivativeCode = toScala(derivative)
+      Source(objectName, s"""
+        package ilc.examples
+        object $objectName {
+          val program = $programCode
+          val derivative = $derivativeCode
+        }
+      """)
+    }
+  }
+
+  case class Source(objectName: String, code: String)
+
+  // if name is "CoolExample", then the generated class will
+  // be "CoolExampleBinary"
+  def addExample(name: String, example: Example) {
+    archive.get(name) match {
+      case Some(example) =>
+        sys error s"example name clash: $name"
+
+      case None =>
+        archive.update(name, example)
+    }
+  }
+}
---
+++ src/main/scala/ilc/examples/Dummy.scala
@@ -0,0 +1,32 @@
+package ilc
+package examples
+
+// ScalaMeter dummy benchmark
+// to help experiment with code generation options
+// and test if everything works together smoothly
+
+trait Dummy {
+  val scalaMeterDummyCode: String =
+    """
+    package ilc
+    package examples
+
+    import org.scalameter.api._
+
+    object DummyBinary extends PerformanceTest.Quickbenchmark {
+      val sizes: Gen[Int] = Gen.range("size")(3000, 15000, 3000)
+
+      val ranges: Gen[Range] = for {
+        size <- sizes
+      } yield 0 until size
+
+      performance of "ilc.examples" in {
+        performance of "Dummy" in {
+          using(ranges) in {
+            r => r.map(_ + 1)
+          }
+        }
+      }
+    }
+    """
+}

043675d869c1830d6d9c82ad5cec3f2fce871e05
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-18 15:46:16 +0200

    [typed-scala] add evaluation of maybe
    Old-commit-hash: 29dd5cf9b8a5140b9ce2a99f31afb54b0864d721

---
+++ src/main/scala/ilc/feature/maybe/Evaluation.scala
@@ -0,0 +1,24 @@
+package ilc
+package feature
+package maybe
+
+trait Evaluation extends functions.Evaluation with Syntax {
+  case class MaybeValue(lift: Option[Value]) extends Value
+
+  override def coreEval(t: Term, env: Env): Value = t match {
+    case Maybe(_, _) =>
+      (z: Value) => (f: Value) => (maybe: Value) => maybe match {
+        case MaybeValue(option) =>
+          option.fold(z) { x => f(x) }
+
+        case _ =>
+          maybe die " should be MaybeValue"
+      }
+    case Nope(_) =>
+      MaybeValue(None)
+    case Just(_) =>
+      (value: Value) => MaybeValue(Some(value))
+    case _ =>
+      super.coreEval(t, env)
+  }
+}

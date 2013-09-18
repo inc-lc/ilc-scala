2fa076960e6c55346870fa559a51d2b38aaa6992
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-18 16:40:58 +0200

    [typed-scala] test reification
    Old-commit-hash: ad30e0b46a4d71460af7378d1852ff2df7d00c43

---
+++ src/test/scala/ilc/language/bacchus/ReificationSuite.scala
@@ -0,0 +1,29 @@
+package ilc
+package language
+package bacchus
+
+import org.scalatest.FunSuite
+import ilc.feature._
+
+class BacchusReificationSuite
+extends FunSuite
+   with Syntax // for mapLiteral
+   with Subjects // for type shorthands
+   with Evaluation
+   with Reification
+{
+  test("can reify natural numbers") {
+    val n: Value = 42
+    assert(eval(reify(n, ℕ)) === n)
+  }
+
+  test("can reify maps") {
+    val valueType = (ℕ ↦ ℕ) ↦ (ℕ ↦ ℕ)
+    val t = mapLiteral(
+      EmptyMap(ℕ, ℕ) -> mapLiteral(0 -> 100),
+      mapLiteral(1 -> 2, 3 -> 4) -> mapLiteral(5 -> 6, 7 -> 8),
+      mapLiteral(20 -> 30) -> EmptyMap(ℕ, ℕ))
+    val value = eval(t)
+    assert(eval(reify(value, valueType)) === value)
+  }
+}

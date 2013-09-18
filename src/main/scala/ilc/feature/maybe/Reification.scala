d1eabf7ca010f5033a052fe18c57bde0c3423eb3
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-18 15:46:31 +0200

    [typed-scala] add reification of maybe
    Old-commit-hash: d25338c912f5989952a694ea3676189b67868d19

---
+++ src/main/scala/ilc/feature/maybe/Reification.scala
@@ -0,0 +1,17 @@
+package ilc
+package feature
+package maybe
+
+trait Reification extends base.Reification with Evaluation {
+  override def reify(value: Value, valueType: Type): Term =
+    (value, valueType) match {
+      case (MaybeValue(None), MaybeType(contentType)) =>
+        Nope(contentType)
+
+      case (MaybeValue(Some(content)), MaybeType(contentType)) =>
+        Just ! reify(content, contentType)
+
+      case _ =>
+        super.reify(value, valueType)
+    }
+}

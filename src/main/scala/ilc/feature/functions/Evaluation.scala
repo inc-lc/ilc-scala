de655e9784b5440636a7f077724ec2e7f4691ed2
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-18 15:23:15 +0200

    [typed-scala] adapt functions.Evaluation to typed syntax
    Minor points:
    
    - write FunctionValue instead of Value.Function to avoid deep-
      mixin-composition on the Value object
    
    - evaluation of variables are moved to base.Evaluation
    
    Old-commit-hash: 2169c8202b82a2387a57a59ad92baadaf5612c61

--- src/main/scala/ilc/feature/functions/Evaluation.scala
+++ src/main/scala/ilc/feature/functions/Evaluation.scala
@@ -14,38 +14,27 @@ trait Evaluation extends base.Evaluation with Syntax {
   override def coreEval(t: Term, env: Env): Value =
     t match {
       case Abs(x, t) =>
-        (arg: Value) => wrapEval(t, env.updated(x, arg))
+        (arg: Value) => wrapEval(t, env.updated(x.getName, arg))
       case App(s, t) =>
         wrapEval(s, env)(wrapEval(t, env))
-      case Var(name) =>
-        env(name) // NoSuchElementException = free var
       case _ =>
         super.coreEval(t, env)
     }
 
-  val Value: FunValues
+  case class FunctionValue(operator: Value => Value) extends Value
 
-  trait FunValues {
-    case class Function(operator: Value => Value) extends Value
-  }
-
-  implicit def liftFunction[T <% Value](f: Value => T): Value =
-    Value.Function(x => f(x))
-    // `f` is written pointfully so that implicit conversion
-    // on the return value of `f` may kick in to make the
-    // whole expression well-typed
-
-  implicit def liftValuePair[S, T]
-    (p: (S, T))
-    (implicit impS: S => Value, impT: T => Value): (Value, Value) =
-      (impS(p._1), impT(p._2))
-
-  implicit class FunOps(value: Value) {
+  implicit class FunctionValueOps(value: Value) {
     // "toFunction"
     def apply(arg: Value): Value =
       value match {
-        case Value.Function(f) => f(arg)
+        case FunctionValue(f) => f(arg)
         case _ => value die("apply", arg)
       }
   }
+
+  implicit def liftFunctionValue[T <% Value](f: Value => T): Value =
+    FunctionValue(x => f(x))
+    // `f` is written pointfully so that implicit conversion
+    // on the return value of `f` may kick in to make the
+    // whole expression well-typed
 }

97054881bed20518dc0c07dac254b7b4018b0e18
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-18 15:18:26 +0200

    [typed-scala] add trinary/ternary union types
    Old-commit-hash: 20a97891242f429f571d0e9945f02ad6c08b797d

--- src/main/scala/ilc/util/UnionType.scala
+++ src/main/scala/ilc/util/UnionType.scala
@@ -17,5 +17,11 @@ package util
 object UnionType {
   private type NOT[P] = P => Nothing
   private type OR[P, Q] = NOT[NOT[P] with NOT[Q]]
+  private type OR3[P, Q, R] = NOT[NOT[P] with NOT[Q] with NOT[R]]
+  private type OR4[P, Q, R, S] =
+    NOT[NOT[P] with NOT[Q] with NOT[R] with NOT[S]]
+
   type Or[S, T] = { type Type[X] = NOT[NOT[X]] <:< OR[S, T] }
+  type Or3[R, S, T] = { type Type[X] = NOT[NOT[X]] <:< OR3[R, S, T] }
+  type Or4[Q, R, S, T] = { type Type[X] = NOT[NOT[X]] <:< OR4[Q, R, S, T] }
 }

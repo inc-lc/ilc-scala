407dd15b6b3c7175f81623e8317d45428d43d4d6
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-18 16:34:46 +0200

    [typed-scala] test changes to maybes
    Old-commit-hash: c7402f1048b40b06dfa8b09f729dbb1c10b237d3

---
+++ src/test/scala/ilc/feature/maybe/DeltaMaybeSuite.scala
@@ -0,0 +1,43 @@
+package ilc
+package feature
+package maybe
+
+import org.scalatest.FunSuite
+
+class DeltaMaybeSuite
+extends FunSuite
+   with ReplacementValues
+   with Evaluation
+   with naturals.ReplacementValues
+   with naturals.Evaluation
+   with sums.Evaluation
+{
+  val ℕ = NatType
+
+  val toNope = Inj2(ℕ) ! Nope(ℕ)
+
+  val to42 = Inj2(ℕ) ! (Just ! 42)
+
+  def toJust(i: Int) = Inj1(MaybeType(ℕ)) ! i
+
+  test("updateTerm behaves as expected") {
+    assert(eval(ChangeUpdate ! toNope ! (Just ! 5)) ===
+      MaybeValue(None))
+
+    assert(eval(ChangeUpdate ! to42 ! Nope(ℕ)) ===
+      MaybeValue(Some(42)))
+
+    assert(eval(ChangeUpdate ! toJust(42) ! (Just ! 5)) ===
+      MaybeValue(Some(42)))
+
+    assert(eval(ChangeUpdate ! toJust(42) ! Nope(ℕ)) ===
+      MaybeValue(None))
+  }
+
+  test("diffTerm produces replacements") {
+    assert(eval(Diff ! Nope(ℕ) ! (Just ! 5)) ===
+      SumValue(Right(MaybeValue(None))))
+    assert(eval(Diff ! (Just ! 5) ! Nope(ℕ)) ===
+      SumValue(Right(MaybeValue(Some(5)))))
+  }
+}

05c9e3594ebb0440828878079ff25b260dfe080a
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-18 16:07:22 +0200

    [typed-scala] adapt maps.Evaluation to typed syntax
    and remove deep mixin composition of the Value object
    
    Old-commit-hash: 15ddf9f9ecd2fbbd7bc9b13fb4d74f573047b5a0

--- src/main/scala/ilc/feature/maps/Evaluation.scala
+++ src/main/scala/ilc/feature/maps/Evaluation.scala
@@ -5,63 +5,61 @@ import scala.language.implicitConversions
 import scala.collection.immutable
 
 // Map evaluation - what only depends on base.Evaluation
-trait EvaluationBase extends feature.base.Evaluation {
+trait EvaluationBase extends feature.base.Evaluation with Syntax {
+  // ValueMap is a map between values.
+  // MapValue is the result of evaluating an expression of MapType.
+
   type ValueMap = immutable.Map[Value, Value]
+
+  case class MapValue(toMap: ValueMap) extends Value
+
+  implicit def liftMapValue(m: ValueMap): Value = MapValue(m)
+
+  object MapValue {
+    def apply[K <% Value, V <% Value](assoc: (K, V)*): MapValue = {
+      val assocValues: Seq[(Value, Value)] = assoc map {
+        case (key, value) => {
+          val keyValue: Value = key
+          val valValue: Value = value
+          (keyValue, valValue)
+        }
+      }
+      MapValue(immutable.Map.apply(assocValues: _*))
+    }
+  }
+
   def ValueMap(assoc: (Value, Value)*): ValueMap =
     immutable.Map.apply[Value, Value](assoc: _*)
 
   implicit class MapOps(value: Value) {
     def toMap: ValueMap =
       value match {
-        case Value.Map(m) => m
+        case MapValue(m) => m
         case _ => value die "toMap"
       }
   }
-
-  implicit def liftMap(m: ValueMap): Value = Value.Map(m)
-
-  // Basic Map values
-  trait MapValues {
-    case class Map(toMap: ValueMap) extends Value
-
-    object Map {
-      def apply(assoc: (Value, Value)*): Map =
-        Map(immutable.Map(assoc: _*))
-    }
-  }
-  val Value: MapValues
 }
 
-trait Evaluation extends EvaluationBase with feature.maybe.SumUnitEncoded {
-  this: Syntax =>
-
-  // the object `Value` needs deep mixin composition.
-  //
-  // the object `Value` serves as a name space so that we can write
-  // `Value.Map`, and it will be distinct from the syntax tree node
-  // `Map` inherted from the `Syntax` trait.
-  //
-  // Question: Is there any way to support namespaces for inner-class-
-  // declarations without doing deep mixin composition?
-
-  val Value: MapValues with MaybeValues
-
-  override def evalConst(c: Constant): Value = c match {
-    case EmptyMap =>
+trait Evaluation extends EvaluationBase with feature.maybe.Evaluation {
+  override def coreEval(t: Term, env: Env): Value = t match {
+    case EmptyMap(_, _) =>
       ValueMap()
 
-    case Update =>
+    case Update(_, _) =>
       (k: Value) => (v: Value) => (m: Value) =>
         m.toMap.updated(k, v)
 
-    case Lookup =>
-      (k: Value) => (m: Value) => lowerMaybe(m.toMap.get(k))
+    case Lookup(_, _) =>
+      (k: Value) => (m: Value) => MaybeValue(m.toMap.get(k))
 
-    case Fold =>
+    case Delete(_, _) =>
+      (k: Value) => (m: Value) => MapValue(m.toMap - k)
+
+    case Fold(_, _, _) =>
       (f: Value) => (z: Value) => (map: Value) =>
         map.toMap.foldRight(z)((p, b) => f(p._1)(p._2)(b))
 
     case _ =>
-      super.evalConst(c)
+      super.coreEval(t, env)
   }
 }

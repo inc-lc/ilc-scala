51ca651dca79889c63ca3af3392320d1cea71a8a
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-18 16:21:58 +0200

    [typed-scala] test detection of type errors
    Old-commit-hash: fcb702c1b8a258aa3b8e778a784834c0ed813c00

---
+++ src/test/scala/ilc/feature/base/TermBuilderSuite.scala
@@ -0,0 +1,36 @@
+package ilc
+package feature
+package base
+
+import org.scalatest.FunSuite
+
+class TermBuilderSuite
+extends FunSuite
+   with base.Syntax
+   with functions.Syntax // for term application operator !
+{
+  object Bot extends Type { override def toString = "⊥" }
+  object Top extends Type { override def toString = "⊤" }
+  case object TT extends Term { def getType: Type = Top }
+
+  test("SpecializedTerm detects mismatched argument type") {
+    val typeError = intercept[TypeError] {
+      SpecializedTerm(Var("x", Top =>: Top)) specialize Bot
+    }
+    info(typeError.getMessage)
+  }
+
+  // id2 : ∀T. (T → T) → (T → T)
+  object id2 extends ConstantWith1TypeParameter {
+    val typeConstructor = TypeConstructor("T") { T =>
+      (T =>: T) =>: (T =>: T)
+    }
+  }
+
+  test("Polymorphic constants detect mismatched argument type") {
+    val typeError = intercept[TypeError] {
+      val t: Term = id2 ! TT
+    }
+    info(typeError.getMessage)
+  }
+}

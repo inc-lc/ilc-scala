9d9c3a79db695825a2069edd321b3a07396f12d9
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-18 15:49:47 +0200

    [typed-scala] add the syntax of natural numbers
    Old-commit-hash: 3682d791ab5b5fa52dadb01526d57303e010d213

--- src/main/scala/ilc/feature/naturals/Syntax.scala
+++ src/main/scala/ilc/feature/naturals/Syntax.scala
@@ -1,18 +1,30 @@
 package ilc
-package feature.naturals
+package feature
+package naturals
 
 import scala.language.implicitConversions
 
-trait Syntax extends feature.functions.Syntax {
+trait Syntax extends base.Syntax with Types {
   // intro/elim forms of nats
   //
-  //   foldNat : r → (r → r) → Nat → r
-  //
-  case class Nat(n: Int) extends Constant { require(n >= 0) }
-  case object FoldNat extends Constant
-  case object Plus extends Constant with NestingBinaryOperator
+  //   foldNat : r → (r → r) → ℕ → r
+
+  case object FoldNat extends ConstantWith1TypeParameter {
+    val typeConstructor = TypeConstructor("r") { r =>
+      r =>: (r =>: r) =>: NatType =>: r
+    }
+  }
+
+  case class Nat(n: Int) extends Term {
+    require(n >= 0)
+    override def getType: Type = NatType
+  }
+
+  case object Plus extends Term {
+    override def getType: Type =
+      NatType =>: NatType =>: NatType
+  }
 
   // implicit conversions
-  implicit def natToConst(n: Int): Constant = Nat(n)
-  implicit def natToTerm(n: Int): Term = Const(natToConst(n))
+  implicit def natToTerm(n: Int): Term = Nat(n)
 }

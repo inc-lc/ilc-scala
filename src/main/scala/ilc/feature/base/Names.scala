e6cf5119a6fd4587fd1b6ec275fac2372338c2f1
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-18 13:54:38 +0200

    [typed-scala] make trait `Name` to abstract over all names
    Reason: protect the following operations from the implementation
    detail that names were strings:
    1. coming up with fresh names
    2. systematically rename variables "x" to "dx" during derivation
    
    Old-commit-hash: 183dfb3c7088e5772fac67b7aeea0d27dd64431f

---
+++ src/main/scala/ilc/feature/base/Names.scala
@@ -0,0 +1,44 @@
+package ilc
+package feature
+package base
+
+/** Declares the abstract class Name and its subclasses.
+  * Since subclasses of Name must have distinct string
+  * representations, they are all declared here.
+  */
+
+import scala.language.implicitConversions
+
+trait Names {
+  sealed trait Name {
+    override def toString: String = ???
+  }
+
+  implicit def stringAsLiteralName(string: String): Name =
+    LiteralName(string)
+
+  // NAME CLASSES IN ALPHABETIC ORDER
+
+  // Delta names ends with 'D'
+  case class DeltaName(original: Name) extends Name
+  {
+    override def toString: String =
+      original.toString + 'D'
+  }
+
+  // Indexed names ends with """_\d+""".r
+  case class IndexedName(original: Name, index: Int) extends Name
+  {
+    override def toString: String =
+      original.toString + "_" + index
+  }
+
+  // Literal names ends with a lower-case letter.
+  case class LiteralName(literal: String) extends Name {
+    override def toString =
+      if (literal matches """.*\p{Alpha}\z""")
+        literal
+      else
+        literal + "lit"
+  }
+}

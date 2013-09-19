9b5cfcce66fec42a08557fcad853faa34e848c36
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-19 12:32:00 +0200

    [typed-scala] enable the test MapSuccBinarySuite
    Old-commit-hash: 4f721f76825b690dc5559382e5c89f4d25c8cec6

--- src/test/scala/ilc/examples/MapSuccBinarySuite.scala
+++ src/test/scala/ilc/examples/MapSuccBinarySuite.scala
@@ -2,31 +2,29 @@ package ilc
 package examples
 
 import org.scalatest.FunSuite
-/*
 import MapSuccBinary._
 
 class MapSuccBinarySuite
 extends FunSuite
-   with MapSuccTypes
 {
   val n = 100
 
   test("the compiled derivative is correct") {
-    val old: Data = (1 to n).map(i => i -> i)(collection.breakOut)
+    val old: InputType = (1 to n).map(i => i -> i)(collection.breakOut)
     val res = program(old)
     changes foreach { change =>
       assert(
-        applyChange(derivative(old)(change), res) ===
-          program(applyChange(change, old)))
+        updateOutput(derivative(old)(change))(res) ===
+          program(updateInput(change)(old)))
     }
   }
 
-  val changes: List[Change] = {
+  val changes: List[DeltaInputType] = {
     List(
       Left(Map.empty),
-      Left(Map(1 -> Right(Right(n + 1)))),
-      Left(Map(n + 2 -> Left(Right(n + 2)))),
-      Left(Map(2 -> Left(Left(())))),
-      Left(Map(n -> Left(Left(())))))
+      Left(Map(1 -> Right(n + 1))),
+      Left(Map(n + 2 -> Left(Some(n + 2)))),
+      Left(Map(2 -> Left(None))),
+      Left(Map(n -> Left(None))))
   }
-}*/
+}

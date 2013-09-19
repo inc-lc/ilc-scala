a5d0fd68ab1a3097befd5d2edf48366114bbbb85
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-09-19 12:26:50 +0200

    [typed-scala] fix type error in generated code by maybe.ToScala
    Old-commit-hash: b576834201113452d2fe8f9028d93ec822673805

--- src/main/scala/ilc/feature/maybe/ToScala.scala
+++ src/main/scala/ilc/feature/maybe/ToScala.scala
@@ -6,9 +6,9 @@ trait ToScala extends base.ToScala with Syntax {
   override def toScala(t: Term): String = t match {
     case Maybe(aType, bType) => {
       val List(a, b) = List(aType, bType) map toScala
-      val maybe_b = toScala(MaybeType(bType))
+      val maybe_a = toScala(MaybeType(aType))
       val body = s"m.fold[$b](z)(f)"
-      s"((z: $b) => (f: $a => $b) => (m: $maybe_b) => $body)"
+      s"((z: $b) => (f: $a => $b) => (m: $maybe_a) => $body)"
     }
 
     case Nope(contentType) => {

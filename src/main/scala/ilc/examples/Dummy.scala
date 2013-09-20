06597b38433bb06a3c8c1e93341da3f11baba41b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-09-20 18:14:56 +0200

    Avoid whitespace in output through stripMargin
    Old-commit-hash: 23121a36fc67219aa2d14c0f3351bc4b7aa4afff

--- src/main/scala/ilc/examples/Dummy.scala
+++ src/main/scala/ilc/examples/Dummy.scala
@@ -7,26 +7,25 @@ package examples
 
 trait Dummy {
   val scalaMeterDummyCode: String =
-    """
-    package ilc
-    package examples
-
-    import org.scalameter.api._
-
-    object DummyBinary extends PerformanceTest.Quickbenchmark {
-      val sizes: Gen[Int] = Gen.range("size")(3000, 15000, 3000)
-
-      val ranges: Gen[Range] = for {
-        size <- sizes
-      } yield 0 until size
-
-      performance of "ilc.examples" in {
-        performance of "Dummy" in {
-          using(ranges) in {
-            r => r.map(_ + 1)
-          }
-        }
-      }
-    }
-    """
+    """|package ilc
+       |package examples
+       |
+       |import org.scalameter.api._
+       |
+       |object DummyBinary extends PerformanceTest.Quickbenchmark {
+       |  val sizes: Gen[Int] = Gen.range("size")(3000, 15000, 3000)
+       |
+       |  val ranges: Gen[Range] = for {
+       |    size <- sizes
+       |  } yield 0 until size
+       |
+       |  performance of "ilc.examples" in {
+       |    performance of "Dummy" in {
+       |      using(ranges) in {
+       |        r => r.map(_ + 1)
+       |      }
+       |    }
+       |  }
+       |}
+       |""".stripMargin
 }

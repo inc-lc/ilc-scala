ee444c7942011d91484f47aee2615510dfe46e1f
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-09-22 22:37:45 +0200

    Move more reusable code out of MapSuccBenchmark
    This code will be reusable for MapSuccFairBenchmark, which will employ
    an auto-generated derivative.
    
    Also, add docs.
    
    View diff with `-C` option.
    
    Old-commit-hash: 6cce3921ffdc883b129f50267798d206cd937fc8

---
+++ src/test/scala/ilc/examples/MapIntIntBenchmark.scala
@@ -0,0 +1,50 @@
+package ilc
+package examples
+
+import scala.collection.breakOut
+import org.scalameter.api.Gen
+
+/**
+  * A template for benchmarking derivatives of generated programs transforming
+  * Map[Int, Int] into Map[Int, Int].
+  */
+abstract class MapIntIntBenchmark(val example: ExampleBinary {
+  type InputType = Map[Int, Int]
+  type OutputType = Map[Int, Int]
+  type DeltaInputType = Either[Map[Int, Either[Option[Int], Int]], Map[Int, Int]]
+  type DeltaOutputType = Either[Map[Int, Either[Option[Int], Int]], Map[Int, Int]]
+}) extends ExampleToBenchmark
+{
+  import example._
+
+  def replacementChange(newInput: Data): Change = Right(newInput)
+
+  // consider leaving out the output.
+  def inputOfSize(n: Int): Data =
+    (1 to n).map(i => (i, i))(breakOut)
+
+  lazy val changeDescriptions: Gen[String] = Gen.enumeration("change")(
+    "no change",
+    "replace 1 by n + 1",
+    "add n + 2",
+    "remove 2",
+    "remove n"
+  )
+
+  def lookupChange(n: Int, description: String): Change = description match {
+    case "no change" =>
+      Left(Map.empty)
+
+    case "replace 1 by n + 1" =>
+      Left(Map(1 -> Right(n + 1)))
+
+    case "add n + 2" =>
+      Left(Map(n + 2 -> Left(Some(n + 2))))
+
+    case "remove 2" =>
+      Left(Map(2 -> Left(None)))
+
+    case "remove n" =>
+      Left(Map(n -> Left(None)))
+  }
+}
--- src/test/scala/ilc/examples/MapSuccBenchmark.scala
+++ src/test/scala/ilc/examples/MapSuccBenchmark.scala
@@ -1,42 +1,8 @@
 package ilc
 package examples
 
-import scala.collection.breakOut
-import org.scalameter.api.Gen
-
-object MapSuccBenchmark extends ExampleToBenchmark
-{
-  val example = ilc.examples.MapSuccBinary
-  import example._
-
-  def replacementChange(newInput: Data): Change = Right(newInput)
-
-  // consider leaving out the output.
-  def inputOfSize(n: Int): Data =
-    (1 to n).map(i => (i, i))(breakOut)
-
-  lazy val changeDescriptions: Gen[String] = Gen.enumeration("change")(
-    "no change",
-    "replace 1 by n + 1",
-    "add n + 2",
-    "remove 2",
-    "remove n"
-  )
-
-  def lookupChange(n: Int, description: String): Change = description match {
-    case "no change" =>
-      Left(Map.empty)
-
-    case "replace 1 by n + 1" =>
-      Left(Map(1 -> Right(n + 1)))
-
-    case "add n + 2" =>
-      Left(Map(n + 2 -> Left(Some(n + 2))))
-
-    case "remove 2" =>
-      Left(Map(2 -> Left(None)))
-
-    case "remove n" =>
-      Left(Map(n -> Left(None)))
-  }
-}
+/**
+  * Benchmark hand-tuned derivative. This shows what we could achieve by
+  * improving our algorithms.
+  */
+object MapSuccBenchmark extends MapIntIntBenchmark(MapSuccBinary)

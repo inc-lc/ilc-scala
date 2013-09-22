82442796f44e8b5bb7e3f43ba2c1a4d6dc06c07c
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-09-22 23:19:26 +0200

    Unnest Example from ilc.examples.Archive
    This allows for simplifications, shown in next commit.
    
    Old-commit-hash: b4d1b4ba50d386c4ebb539dd94864422e9069f8c

--- src/main/scala/ilc/examples/Archive.scala
+++ src/main/scala/ilc/examples/Archive.scala
@@ -9,50 +9,6 @@ trait Archive {
   def get(exampleWithName: String): Example =
     archive(exampleWithName)
 
-  /** Base class for implementations of examples.
-    * When actually writing examples,
-    * this must be mixed in with versions of ToScala and Derivation
-    * corresponding to the used language features, and with the appropriate
-    * syntax.
-    */
-  abstract class Example
-  {
-    this: feature.base.ToScala
-     with feature.base.Derivation =>
-
-    def program: Term
-    def derivative: Term = derive(program)
-
-    def toSource(name: String): Source = {
-      val objectName = name ++ "Binary"
-      val programCode = toScala(program)
-      val derivativeCode = toScala(derivative)
-      val inputType =>: outputType = program.getType
-      val updateInputCode = toScala(updateTerm(inputType))
-      val updateOutputCode = toScala(updateTerm(outputType))
-      val inputTypeCode = toScala(inputType)
-      val outputTypeCode = toScala(outputType)
-      val deltaInputTypeCode = toScala(deltaType(inputType))
-      val deltaOutputTypeCode = toScala(deltaType(outputType))
-      Source(objectName, s"""
-        package ilc.examples
-        object $objectName extends ExampleBinary {
-          override val program = $programCode
-          override val derivative = $derivativeCode
-          override val updateInput = $updateInputCode
-          override val updateOutput = $updateOutputCode
-
-          type InputType = $inputTypeCode
-          type OutputType = $outputTypeCode
-          type DeltaInputType = $deltaInputTypeCode
-          type DeltaOutputType = $deltaOutputTypeCode
-        }
-      """)
-    }
-  }
-
-  case class Source(objectName: String, code: String)
-
   // if name is "CoolExample", then the generated class will
   // be "CoolExampleBinary"
   def addExample(name: String, example: Example) {
@@ -65,3 +21,47 @@ trait Archive {
     }
   }
 }
+
+/** Base class for implementations of examples.
+  * When actually writing examples,
+  * this must be mixed in with versions of ToScala and Derivation
+  * corresponding to the used language features, and with the appropriate
+  * syntax.
+  */
+abstract class Example
+{
+  this: feature.base.ToScala
+   with feature.base.Derivation =>
+
+  def program: Term
+  def derivative: Term = derive(program)
+
+  def toSource(name: String): Source = {
+    val objectName = name ++ "Binary"
+    val programCode = toScala(program)
+    val derivativeCode = toScala(derivative)
+    val inputType =>: outputType = program.getType
+    val updateInputCode = toScala(updateTerm(inputType))
+    val updateOutputCode = toScala(updateTerm(outputType))
+    val inputTypeCode = toScala(inputType)
+    val outputTypeCode = toScala(outputType)
+    val deltaInputTypeCode = toScala(deltaType(inputType))
+    val deltaOutputTypeCode = toScala(deltaType(outputType))
+    Source(objectName, s"""
+      package ilc.examples
+      object $objectName extends ExampleBinary {
+        override val program = $programCode
+        override val derivative = $derivativeCode
+        override val updateInput = $updateInputCode
+        override val updateOutput = $updateOutputCode
+
+        type InputType = $inputTypeCode
+        type OutputType = $outputTypeCode
+        type DeltaInputType = $deltaInputTypeCode
+        type DeltaOutputType = $deltaOutputTypeCode
+      }
+    """)
+  }
+}
+
+case class Source(objectName: String, code: String)

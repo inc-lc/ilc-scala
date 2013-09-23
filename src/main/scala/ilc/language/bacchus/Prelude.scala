cf21a79f4c40353d132bc27c759526501e58835d
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-09-23 19:18:08 +0200

    Clarify inspiration of bacchus.Prelude
    Old-commit-hash: bc52236208131629a0d9e17b01ff73ccd935127a

--- src/main/scala/ilc/language/bacchus/Prelude.scala
+++ src/main/scala/ilc/language/bacchus/Prelude.scala
@@ -6,6 +6,8 @@ package bacchus
   * This trait includes generally useful functions for the Bacchus object
   * language. When Bacchus abstraction is insufficient, we resort to meta-level
   * abstraction (see for instance const).
+  *
+  * Many function are inspired (to some extent) from Haskell ones.
   */
 trait Prelude extends bacchus.Syntax
 {

2c6d0811f9b42b5274c114ed4625ab9e50aa086b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-09-25 01:09:40 +0200

    Add benchmark using the example
    I split this out because the benchmark doesn't work.
    
    Old-commit-hash: 10eb1cf9f024ab294726be29efdd526ecfa00790

---
+++ src/test/scala/ilc/examples/bench/MapSuccBagsBenchmark.scala
@@ -0,0 +1,8 @@
+package ilc
+package examples
+package bench
+
+/**
+  * Benchmark generated derivative.
+  */
+object MapSuccBagsBenchmark extends ExampleToBenchmark(new BagIntBenchData(MapSuccBagsGenerated))

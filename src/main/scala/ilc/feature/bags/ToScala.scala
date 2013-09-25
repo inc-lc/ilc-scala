05bad1d81e26e560f300c84c058193eb7eb25f1d
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-09-25 09:42:13 +0200

    Split bags.Library into its own file.
    View diff with -C30% to confirm no code was added.
    
    Old-commit-hash: e6a141f6ddd207cb053f3e004824224aebaaef39

--- src/main/scala/ilc/feature/bags/ToScala.scala
+++ src/main/scala/ilc/feature/bags/ToScala.scala
@@ -2,34 +2,6 @@ package ilc
 package feature
 package bags
 
-trait BagLibrary {
-  type Bag[T] = Map[T, Int]
-
-  def bagEmpty[T]: Bag[T] = Map.empty[T, Int]
-
-  def bagSingleton[T](t: T): Bag[T] = Map(t -> 1)
-
-  def bagUnion[T](b1: Bag[T])(b2: Bag[T]) =
-    b1 ++ (for ((el, count) <- b2) yield {
-      val newCount =
-        if (b1 contains el)
-          count + b1(el)
-        else
-          count
-      el -> newCount
-    })
-
-  def bagFoldGroup[G, T](op: G => G => G)(inv: G => G)(neutral: G)(f: T => G)(bag: Bag[T]): G = {
-    (for {
-      (t, count) <- bag
-      g = f(t)
-      e <- Seq.tabulate(count)(_ => g)
-    } yield e).fold[G](neutral)(Function.uncurried(op))
-  }
-
-  def bagNegate[T](b: Bag[T]): Bag[T] = b mapValues (-_)
-}
-
 trait ToScala extends base.ToScala with Syntax {
   private[this] def mapTypes(v: Type): (String, String) = {
     val vType = toScala(v)

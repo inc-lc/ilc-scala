f47d882d9563203b3c7876592ec9950492f45a16
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-09-26 20:36:29 +0200

    Other unhelpful attempts
    Try changing the fold direction, and removing elements with a zero
    multiplicity.
    
    Old-commit-hash: 2677ac3b8d15cb7efc50c7b6168c682ad1ace4f5

--- src/main/scala/ilc/feature/bags/Library.scala
+++ src/main/scala/ilc/feature/bags/Library.scala
@@ -9,14 +9,21 @@ trait Library {
 
   def bagSingleton[T](t: T): Bag[T] = Map(t -> 1)
 
+  //XXX: Could be made much faster by using builders (avoiding immutable
+  //copies), but this shouldn't be necessary.
   def bagUnion[T](b1: Bag[T])(b2: Bag[T]) =
-    b1 ++ (for ((el, count) <- b2) yield {
+    b2 ++ (b1 flatMap {
+      case (el, count) =>
+
       val newCount =
         if (b1 contains el)
           count + b1(el)
         else
           count
-      el -> newCount
+      if (newCount == 0)
+        List.empty
+      else
+        List(el -> newCount)
     })
 
   def bagFoldGroup[G, T](op: G => G => G)(inv: G => G)(neutral: G)(f: T => G)(bag: Bag[T]): G = {
@@ -25,7 +32,7 @@ trait Library {
       g = f(t)
       (h, posCount) = if (count >= 0) (g, count) else (inv(g), -count)
       e <- Seq.tabulate(posCount)(_ => h)
-    } yield e).fold[G](neutral)(Function.uncurried(op))
+    } yield e).foldRight[G](neutral)(Function.uncurried(op))
   }
 
   def bagNegate[T](b: Bag[T]): Bag[T] = b mapValues (-_)

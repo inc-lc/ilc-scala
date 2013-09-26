df2c4caa23177ca3070894a9e51a787dd2f0e3f0
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-09-27 00:49:56 +0200

    Increase collection sizes for bags
    With the previous performance improvements, a slightly bigger size is
    acceptable. However, we still aren't on par with other collections.
    
    Old-commit-hash: e684444530a80373e5b7d24e6e6617cd6c3141f5

--- src/test/scala/ilc/examples/bench/MapSuccBagsBenchmark.scala
+++ src/test/scala/ilc/examples/bench/MapSuccBagsBenchmark.scala
@@ -7,7 +7,7 @@ package bench
   */
 object MapSuccBagsBenchmark extends NonReplacementChangeBenchmark(
   new BagIntBenchData(MapSuccBagsGenerated) {
-    override def base = 10
-    override def last = 50
-    override def step = 10
+    override def base = 50
+    override def last = 250
+    override def step = 50
   })

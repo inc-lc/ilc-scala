9c636e9d36faf00e9cf08ae212c2ab6244fa191c
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-10-02 00:22:48 +0200

    separate base.TypeConstructor trait
    Because inverting type function is very useful and we don't want
    to confine it to inside a polymorphic term.
    
    Conflicts:
    	scala-prototype/src/main/scala/ilc/feature/base/Syntax.scala
    
    (Note by PG: this drops uses of getConstantName in error messages.
    The merge conflict was against cddc1897c6000a4ced4be0890f12b9db96479c55,
    but that change does not carry over directly because of these other
    changes to error messages).
    
    Old-commit-hash: 8eb7d720e13e261a63a0f685c3a4881001014154

--- src/main/scala/ilc/feature/base/Types.scala
+++ src/main/scala/ilc/feature/base/Types.scala
@@ -3,5 +3,23 @@ package feature
 package base
 
 trait Types {
+  // We want subclasses of Type to be case classes.
+  // If subclass is not a case class/case object, then they
+  // fail to intantiate at compile time (a good thing) with
+  // an obscure error message (a necessary evil).
   trait Type extends Product
+
+  // ERROR THROWERS
+
+  def typeErrorNotTheSame(context: String, expected: Any, actual: Any) =
+    throw TypeError(s"expected $expected instead of $actual in $context")
+
+  def typeErrorNotDefined(name : Any) =
+    throw TypeError("undefined identifier " + name)
 }
+
+case class TypeError(msg: String)
+extends Exception(s"Type error: $msg")
+
+case class IDontKnow(what: String)
+extends Exception(s"I don't know $what")

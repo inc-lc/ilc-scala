6e783c6fbffd936c3872b9674f1caffe60b554fb
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-10-01 12:34:24 +0200

    change the syntax of abelian maps to use abelian groups
    `foldGroup` is renamed to `foldByHom` and takes two groups as
    arguments. The value group of the input map is there for security:
    If the derivative of `foldByHom` is given a change in terms of
    a group other than the one declared as the domain of the homomorphism,
    then it should recompute.
    
    Old-commit-hash: fd0b7073642297cdd94a6c46cba5da3cbee4bd5d

--- src/main/scala/ilc/feature/abelianMaps/Syntax.scala
+++ src/main/scala/ilc/feature/abelianMaps/Syntax.scala
@@ -1,26 +1,21 @@
 package ilc
 package feature
-package mapReduce
+package abelianMaps
 
 trait Syntax
 extends maps.Syntax
-   with maps.Types
-   with maybe.Types
-   with products.Types
+   with abelians.Types
 {
-  // intro/elim forms of maps for mapReduce (of base-type values)
+  // intro/elim forms of maps with abelian groups on values
   //
   // (new constructors)
   //
   //   singleton : k → v → Map k v
   //
-  //   combine : (v → v → Maybe v) → Map k v → Map k v
+  //   liftGroup : Abelian v → Abelian (Map k v)
   //
-  //   foldGroup : (op : b → b → b) → (inv : b → b) → (e : b) →
+  //   foldByHom : Abelian a → Abelian b →
   //               (k → a → b) → Map k a → b
-  //
-  // (It is not possible to encode combine with foldGroup because
-  // there is no other means to enlarge the map.)
   //
   // (inherited from feature.maps)
   //
@@ -35,17 +30,38 @@ extends maps.Syntax
     }
   }
 
-  object Combine extends ConstantWith2TypeParameters {
-    val typeConstructor = TypeConstructor("k", "v") {
-      case Seq(k, v) =>
-        (v =>: v =>: MaybeType(v)) =>: MapType(k, v) =>: MapType(k, v)
+  object LiftGroup extends ConstantWith2TypeParameters {
+    val typeConstructor = TypeConstructor("keyType", "valType") {
+      case Seq(keyType, valType) =>
+        AbelianType(valType) =>: AbelianType(MapType(keyType, valType))
+    }
+
+    /** Give only the key type of LiftGroup, deduce valType from
+      * argument
+      * {{{
+      * (LiftGroup(Word) ! additiveGroup).getType =
+      *   AbelianType(MapType(Word, IntType))
+      * }}}
+      * Although this pattern of partially specifying type
+      * arguments might be abstracted in traits, it's probably
+      * more productive to simply code unification (in Haskell,
+      * it's just a couple dozen lines).
+      */
+    def apply(keyType: Type): PolymorphicTerm = new PolymorphicTerm {
+      def specialize(argumentTypes: Type*): Term = {
+        val Seq(valType) =
+          TypeConstructor("v")(AbelianType.apply _).
+            inverse(argumentTypes.head)
+        LiftGroup(keyType, valType)
+      }
     }
   }
 
-  case object FoldGroupMap extends ConstantWith3TypeParameters {
-    val typeConstructor = TypeConstructor("b", "k", "v") {
-      case Seq(b, k, v) =>
-        (b =>: b =>: b) =>: (b =>: b) =>: b =>: (k =>: v =>: b) =>: MapType(k, v) =>: b
+  object FoldByHom extends ConstantWith3TypeParameters {
+    val typeConstructor = TypeConstructor("k", "a", "b") {
+      case Seq(k, a, b) =>
+        AbelianType(a) =>: AbelianType(b) =>:
+          (k =>: a =>: b) =>: MapType(k, a) =>: b
     }
   }
 }

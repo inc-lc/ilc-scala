446e61ded8986cb4ce55bee9de2226bf9d41a540
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-10-01 19:15:04 +0200

    add replacement changes for abelian groups
    Old-commit-hash: fd23dbe56c5ea71ef109aaca037030b9b01f1404

---
+++ src/main/scala/ilc/feature/abelianGroups/Derivation.scala
@@ -0,0 +1,36 @@
+package ilc
+package feature
+package abelianGroups
+
+/** The change to a group is a replacement.
+  *
+  *   Δ Abelian(e) = Abelian(e)
+  */
+
+trait Derivation extends base.Derivation with Syntax {
+  override def deltaType(tau: Type): Type = tau match {
+    case AbelianGroupType(_) =>
+      tau
+
+    case _ =>
+      super.deltaType(tau)
+  }
+
+  override def updateTerm(tau: Type): Term = tau match {
+    case _G @ AbelianGroupType(_) =>
+      lambda(_G, _G) { case Seq(dG, _G) => dG }
+
+    case _ =>
+      super.updateTerm(tau)
+  }
+
+  override def diffTerm(tau: Type): Term = tau match {
+    case _G @ AbelianGroupType(_) =>
+      lambda(_G, _G) { case Seq(_Gnew, _Gold) => _Gnew }
+
+    case _ =>
+      super.updateTerm(tau)
+  }
+
+  // do not override derive, do replacement in all cases
+}

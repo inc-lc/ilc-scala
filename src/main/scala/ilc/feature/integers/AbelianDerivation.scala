7ef05349e2eae30e261a4d3098fa506dde70b918
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-10-03 16:50:28 +0200

    rephrase delta-integer in abelianGroups.AbelianDerivation framework
    Old-commit-hash: cf302dd182ac2afec504a06c19cb8960e54c924b

--- src/main/scala/ilc/feature/integers/AbelianDerivation.scala
+++ src/main/scala/ilc/feature/integers/AbelianDerivation.scala
@@ -8,65 +8,23 @@ package integers
   * replacement.
   */
 trait AbelianDerivation
-extends base.Derivation
+extends abelianGroups.AbelianDerivation
    with Syntax
-   with functions.Derivation // for `lambdaDelta`
-   with abelianGroups.Derivation
-   with products.Syntax
-   with sums.SyntaxSugar
+   with functions.ContextSensitiveDerivation
 {
-  private def deltaInt    = ProductType(AbelianGroupType(IntType), IntType)
-  private def getBinaryOp = GetBinOp   composeWith Proj1%deltaInt
-  private def getInverse  = GetInv     composeWith Proj1%deltaInt
-  private def getNeutral  = GetNeutral composeWith Proj1%deltaInt
-  private def getElement  = Proj2%deltaInt
-  private def replaceBy   = Inj2(deltaInt, IntType)
-
-  override def deltaType(tau: Type): Type = tau match {
-    case IntType =>
-      SumType(deltaInt, IntType)
-
-    case _ =>
-      super.deltaType(tau)
+  override def isAbelianType(tau: Type): Boolean = tau match {
+    case IntType => true
+    case _       => super.isAbelianType(tau)
   }
 
-  override def updateTerm(tau: Type): Term = tau match {
-    case IntType => {
-      lambda(Var("dn", deltaType(IntType)), Var("n", IntType)) {
-        case Seq(dn, n) =>
-          case2(dn,
-            // group element
-            lambda(deltaInt) { groupElem =>
-              getBinaryOp ! groupElem ! (getElement ! groupElem) ! n
-            },
-            // replacement
-            lambda(Var("replacement", IntType)) {x => x})
-      }
-    }
-
-    case _ =>
-      super.updateTerm(tau)
-  }
-
-  override def diffTerm(tau: Type): Term = tau match {
-    case IntType =>
-      // return replacement always
-      lambda(IntType, IntType) { case Seq(m, n) =>
-        replaceBy ! m
-      }
-
-    case _ =>
-      super.diffTerm(tau)
-  }
-
-  override def derive(t: Term): Term = t match {
-    case LiteralInt(i) =>
-      replaceBy ! t
+  override def deriveSubterm(s: Subterm): Term = s.toTerm match {
+    case term @ LiteralInt(i) =>
+      replacementChange ! term
 
     // PlusInt and NegateInt can use the slow derivative. It operates on
     // integers, so calling `updateTerm` and `diffTerm` is okay.
 
     case _ =>
-      super.derive(t)
+      super.deriveSubterm(s)
   }
 }

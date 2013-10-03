b7f7316d0363fb0d55c6f00fa424ae7e1d50fcf0
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-10-03 22:40:57 +0200

    automatize MapSuccExample
    Old-commit-hash: e3b6a9ffd3ca4f08d08314514a4dd7ac9e85d60b

--- src/main/scala/ilc/examples/MapSuccExample.scala
+++ src/main/scala/ilc/examples/MapSuccExample.scala
@@ -1,47 +1,23 @@
 package ilc
 package examples
 
-import language.bacchus
+import feature._
 
-class MapSuccExample extends MapSuccBaseExample {
-  override def derivative: Term = dmap_s1 ! constSucc ! (derive(constSucc))
+class MapSuccExample
+extends Example
+   with bags.SyntaxSugar
 
-  // hand-crafted derivative of map with the first argument stable
-  // specialized to the type (ℕ → ℕ → ℕ) → Map ℕ ℕ → Map ℕ ℕ
-  def dmap_s1: Term = {
-    val mType = MapType(ℕ, ℕ)
-    val fVar = Var("f", ℕ =>: ℕ =>: ℕ)
-    val mVar = Var("m", mType)
-    lambda(fVar, DVar(fVar), mVar, DVar(mVar)) { case Seq(f, df, m, dm) =>
-      Either !
-        // if the change is insertion/deletion/modification,
-        // then map over the changes and see what happens.
-        (Inj1(mType) composeWith (mapWithKey ! lambda(Var("key", ℕ)) { key =>
-          Either !
-            // case del/ins
-            // -- on deletion, delete it anyway
-            // -- on insertion, insert the new value
-            (Inj1(ℕ) composeWith (Maybe !
-              Nope(ℕ) !
-              (Just composeWith (f ! key))
-            )) !
-            // case update:
-            // 1. apply the given derivative `df`
-            // 2. wrap result change in an ins/del/mod type
-            (Inj2(MaybeType(ℕ)) composeWith (df ! key ! key ! (
-              // TODO: replace 1997 by ErrorTerm
-              // TODO2: lift this maybe-free lookup term as sugar
-              Maybe ! 1997 !
-                lambda(ℕ) { x => x } !
-                (Lookup ! key ! m)
-            )))
-        })) !
-        // if the change is a replacement,
-        // then increment the replacement.
-        lambda(mType) { newMap =>
-          mkMapReplacement(mapWithKey ! f ! newMap)
-        } !
-        dm
-    }
-  }
+   // context-sensitive derivations
+   with bags.AbelianDerivation
+   with integers.AbelianDerivation
+
+   // code generation
+   with bags.ToScala
+   with booleans.ToScala
+   with functions.ToScala
+   with integers.ToScala
+   with products.ToScala
+   with sums.ToScala
+{
+  def program: Term = map ! (PlusInt ! LiteralInt(1))
 }

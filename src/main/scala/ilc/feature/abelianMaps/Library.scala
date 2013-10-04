a8dec78112413d7af53737d963951b52b11579f1
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-10-04 20:04:24 +0200

    consider compiling abelian maps to HashMap
    Syntax of lifted map inversion is different because the old
    method of lifting map inversion is `mapValues`, who cannot
    construct a HashMap from another HashMap, because it takes
    no CanBuildFrom implicit argument.
    
    Old-commit-hash: 4e11c4236b5651109b088629d94a42dbe34ae984

--- src/main/scala/ilc/feature/abelianMaps/Library.scala
+++ src/main/scala/ilc/feature/abelianMaps/Library.scala
@@ -2,16 +2,17 @@ package ilc
 package feature
 package abelianMaps
 
+import collection.immutable.HashMap
 import abelianGroups.Library._
 
 object Library {
   case class LiftedMapGroup[K, V](valueGroup: AbelianGroup[V])
-  extends AbelianGroup[Map[K, V]]
+  extends AbelianGroup[AbelianMap[K, V]]
   {
-    private type T = Map[K, V]
+    private type T = AbelianMap[K, V]
 
     val binOp: T => T => T = m1 => m2 => {
-      // consider using a mapBuilder instead (compare benchmarks)
+      // consider using HashMap.merge instead (compare benchmarks)
       m1.foldRight[T](m2) { (keyValuePair, wipMap) => {
         val (key, value) = keyValuePair
         val newValue = wipMap.get(key).fold[V](value) {
@@ -24,9 +25,11 @@ object Library {
       }
     }}
 
-    val inv: T => T = _.mapValues[V](valueGroup.inv)
+    val inv: T => T = _.map { case (key, value) =>
+      (key, valueGroup.inv(value))
+    }
 
-    val neutral: T = Map.empty
+    val neutral: T = AbelianMap.empty
 
     def isEqualGroup(that: AbelianGroup[T]): Boolean = that match {
       case that @ LiftedMapGroup(_) =>

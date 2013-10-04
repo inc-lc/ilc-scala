1794874a9e90c16add46abe815c0f6c5dfb8c4d2
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-10-04 20:08:19 +0200

    generate code for abelian maps
    Old-commit-hash: 1d974b6632048189d0ba6003d0083b8a65a3f41b

---
+++ src/main/scala/ilc/feature/abelianMaps/ToScala.scala
@@ -0,0 +1,25 @@
+package ilc
+package feature
+package abelianMaps
+
+trait ToScala extends maps.ToScala with Syntax {
+  addLibrary("abelianMaps")
+
+  override def toScala(tau: Type): String = tau match {
+    case MapType(keyType, valType) =>
+      s"AbelianMap[${toScala(keyType)}, ${toScala(valType)}]"
+
+    case _ =>
+      super.toScala(tau)
+  }
+
+  override def toUntypedScala(t: Term): String = t match {
+    case EmptyMap(k, v)     => "emptyMap"
+    case SingletonMap(k, v) => "singletonMap"
+    case LiftGroup(k, v)    => "liftGroup"
+    case FoldByHom(k, a, b) => "foldByHom"
+
+    case _ =>
+      super.toUntypedScala(t)
+  }
+}

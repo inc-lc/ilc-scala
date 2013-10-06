06d9d657690d2e668e7a201346c0f974b97e8a1c
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-10-07 00:18:34 +0200

    test histogram example
    Old-commit-hash: d5e85aeb375f4a82b11b4d8958af36aa29da875f

---
+++ src/test/scala/ilc/examples/HistogramGeneratedSuite.scala
@@ -0,0 +1,30 @@
+package ilc
+package examples
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import ilc.examples.HistogramGenerated._
+import ilc.feature.abelianMaps.Library._
+import ilc.feature.bags.Library._
+
+class HistogramGeneratedSuite
+extends FunSuite
+   with ShouldMatchers
+{
+  val input: InputType =
+    AbelianMap(
+      1 -> Bag(1, 2, 3, 4, 5),
+      2 -> Bag(   2, 3, 4   ),
+      3 -> Bag(      3      )
+    )
+
+  test("Histogram is correct on a toy example") {
+    program(input) should be(AbelianMap(
+      1 -> 1,
+      2 -> 2,
+      3 -> 3,
+      4 -> 2,
+      5 -> 1
+    ))
+  }
+}

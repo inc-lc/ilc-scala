264434cca590bc43c2abc225a044da9a7aa9ca56
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-10-07 19:15:06 +0200

    Fix substitution part 4
    Ensure fresh variables are really fresh!
    
    Old-commit-hash: 26a880b6b4f74059b2bf75296d82b5c1140b00f2

--- src/main/scala/ilc/feature/functions/BetaReduction.scala
+++ src/main/scala/ilc/feature/functions/BetaReduction.scala
@@ -2,7 +2,7 @@ package ilc
 package feature
 package functions
 
-trait BetaReduction extends Syntax {
+trait BetaReduction extends Syntax with analysis.FreeVariables {
   def subst(v: Variable, arg: Term, typingContext: TypingContext): Term => Term = {
     def go(body: Term): Term =
       body match {
@@ -15,9 +15,15 @@ trait BetaReduction extends Syntax {
           if (v == v2) //v is shadowed
             body
           else { // (v != v2)
-            //XXX: ensure this is not free in arg.
-            val v3 = Var(freshName(typingContext, v2.getName), v2.getType)
-            val extContext = v2 +: typingContext
+            val freeVars = arg.freeVariables
+            val extendedTypingContext = v2 +: TypingContext(typingContext.toList ++ freeVars)
+            val v3 = Var(freshName(extendedTypingContext, v2.getName), v2.getType)
+            ////XXX this might be bigger than needed.
+            //val extContext = v3 +: extendedTypingContext
+
+            //This typing context needs to be bigger than usual, since we're
+            //moving the term to a different lambda context.
+            val extContext = v3 +: v2 +: typingContext
             val alphaRenamedBody2 = subst(v2, v3, extContext)(body2)
             Abs(v3, subst(v, arg, extContext)(alphaRenamedBody2))
           }

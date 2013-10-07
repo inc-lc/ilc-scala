c93d57ed8f0363f0187fd0ee0d45dedb1261ce75
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-10-07 19:52:24 +0200

    Refactor substitution: use readable variable names
    Old-commit-hash: 69556c4776b557f28f5befd54ef83d07d9b19a4b

--- src/main/scala/ilc/feature/functions/BetaReduction.scala
+++ src/main/scala/ilc/feature/functions/BetaReduction.scala
@@ -3,33 +3,33 @@ package feature
 package functions
 
 trait BetaReduction extends Syntax with analysis.FreeVariables {
-  def subst(v: Variable, arg: Term, typingContext: TypingContext): Term => Term = {
-    def go(body: Term): Term =
-      body match {
-        case v2: Variable =>
-          if (v == v2)
-            arg
+  def subst(toReplace: Variable, replacement: Term, typingContext: TypingContext): Term => Term = {
+    def go(replaceIn: Term): Term =
+      replaceIn match {
+        case v: Variable =>
+          if (toReplace == v)
+            replacement
           else
-            body
-        case Abs(v2, body2) =>
-          if (v == v2) //v is shadowed
-            body
-          else { // (v != v2)
-            val freeVars = arg.freeVariables
-            val extendedTypingContext = v2 +: TypingContext(typingContext.toList ++ freeVars)
-            val v3 = Var(freshName(extendedTypingContext, v2.getName), v2.getType)
+            replaceIn
+        case Abs(v, body) =>
+          if (toReplace == v) //toReplace is shadowed
+            replaceIn
+          else { // (toReplace != v)
+            val freeVars = replacement.freeVariables
+            val extendedTypingContext = v +: TypingContext(typingContext.toList ++ freeVars)
+            val vPrime = Var(freshName(extendedTypingContext, v.getName), v.getType)
             ////XXX this might be bigger than needed.
-            //val extContext = v3 +: extendedTypingContext
+            //val extContext = vPrime +: extendedTypingContext
 
             //This typing context needs to be bigger than usual, since we're
             //moving the term to a different lambda context.
-            val extContext = v3 +: v2 +: typingContext
-            val alphaRenamedBody2 = subst(v2, v3, extContext)(body2)
-            Abs(v3, subst(v, arg, extContext)(alphaRenamedBody2))
+            val extContext = vPrime +: v +: typingContext
+            val alphaRenamedBody = subst(v, vPrime, extContext)(body)
+            Abs(vPrime, subst(toReplace, replacement, extContext)(alphaRenamedBody))
           }
-        case App(fun, arg2) =>
-          App(go(fun), go(arg2))
-        case _ => body
+        case App(fun, arg) =>
+          App(go(fun), go(arg))
+        case _ => replaceIn
       }
     go _
   }

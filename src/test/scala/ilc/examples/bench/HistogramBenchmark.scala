7c621512c749edccf974a8f2203569b2800ac9b8
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-10-09 12:29:08 +0200

    improve benchmark
    1. when given `n`, the generated input has `n` integers in the bags
    
    2. the integers are random between 1 and sqrt n.
    
    Old-commit-hash: 13a97c1ac26bbd1276be56c52365391c4c854602

--- src/test/scala/ilc/examples/bench/HistogramBenchmark.scala
+++ src/test/scala/ilc/examples/bench/HistogramBenchmark.scala
@@ -7,24 +7,31 @@ import org.scalameter.api.Gen
 import ilc.feature.abelianMaps.Library._
 import ilc.feature.abelianGroups.Library._
 import ilc.feature.bags.Library._
-import ilc.feature.bags.BagChanges
 
 object HistogramBenchmark extends NonReplacementChangeBenchmark(
-  new PyramidBenchData(HistogramGenerated) {
+  new BootBenchData(HistogramGenerated) {
     override def base = 50
     override def last = 250
     override def step = 50
   })
 
 object HistogramVerification extends BenchmarkVerification(
-  new PyramidBenchData(HistogramGenerated) {
+  new BootBenchData(HistogramGenerated) {
     override def base = 5
     override def last = 5
     override def step = 5
   })
 
 
-class PyramidBenchData(val example: ExampleGenerated {
+/** Given `n`, create a hash trie of bags with `n` random numbers
+  * in total.
+  *
+  * The second to the last bag gets the same amount of numbers,
+  * the first bag gets the rest, which is is usually more.
+  * If we make a bar chart of the size of bags agains the orginal
+  * numbers of bags, then it looks like a boot facing right.
+  */
+class BootBenchData(val example: ExampleGenerated {
   type InputType = AbelianMap[Int, Bag[Int]]
   type DeltaInputType =
     Either[(AbelianGroup[InputType], InputType), InputType]
@@ -33,35 +40,51 @@ class PyramidBenchData(val example: ExampleGenerated {
   type DeltaOutputType =
     Either[(AbelianGroup[OutputType], OutputType), OutputType]
 })
-extends BenchData with BagChanges
+extends BenchData
+   // a hack to use MapChanges: depends on that both bags and
+   // abelian maps are HashMap when generated.
+   with feature.abelianMaps.MapChanges
 {
   import example._
 
   val numberOfChanges = 10
 
+  def rand1(n: Int): Int = Random.nextInt(n) + 1
+
+  def randomBag(size: Int, ceiling: Int): Bag[Int] =
+    Bag(Seq.fill(size)(rand1(ceiling)): _*)
+
   def changeDescriptions: org.scalameter.api.Gen[String] =
-    Gen.enumeration("change")(changesToBagsOfIntegers.keySet.toSeq: _*)
+    Gen.enumeration("change")(changesToMapsBetweenIntegers.keySet.toSeq: _*)
 
   def inputOfSize(n: Int): InputType = {
-    var bag: Bag[Int] = Bag()
+    val numberOfBags = rand1(n)
+    val numberPerBag = n / numberOfBags
+    val firstBagSize = n - numberPerBag * (numberOfBags - 1)
+    val randMax = math.max(math.sqrt(n.toDouble).toInt, 10)
     AbelianMap(
-      (1 to n) map { i =>
-        // breaks abstraction barrier.
-        // needs abstraction in library for generated code?
-        bag = bag.updated(i, 1)
-        i -> bag
-      }: _*)
+      (2 to n).map(_ -> randomBag(numberPerBag, randMax)).toSeq: _*
+    ).updated(1, randomBag(firstBagSize, randMax))
   }
 
   /** apply the change to a random element in input */
   def lookupChange(desc: String, input: InputType, output: OutputType):
       DeltaInputType = {
     val n = input.size
-    val luckyContestant = Random.nextInt(n) + 1
+    val chosenKey = rand1(input.size)
+    val chosenBag = input(chosenKey)
     Left((
       LiftedMapGroup[Int, Bag[Int]](FreeAbelianGroup()),
-      AbelianMap(luckyContestant ->
-        changesToBagsOfIntegers(desc)(n).fold[Bag[Int]](_._2,
+      AbelianMap(chosenKey ->
+        // maps between integers happen to be bags of integers, too
+        changesToMapsBetweenIntegers(desc)(
+          // the index of this group is unused and can be anything
+          // but the group itself is important, because lifting
+          // the additive group on integers yields an abelian
+          // group on maps that is identical to the free abelian
+          // group of integers, i. e., bags.
+          IndexedGroup.apply[Int](42, x => y => x + y, -_, 0)
+        )(chosenBag).fold[Bag[Int]](_._2,
           _ => sys error "I want group-based bag changes" +
                          " but got a replacement"))
     ))

cdd55a3f26b15cce7c3d9753afee34a7e69d4af1
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-10-11 10:10:54 +0200

    Add Scalameter reporter producing charts
    Old-commit-hash: ef4ccd995c490800ac6b033e8792a9f9cb419a17

--- src/test/scala/ilc/examples/bench/BenchSuite.scala
+++ src/test/scala/ilc/examples/bench/BenchSuite.scala
@@ -2,9 +2,14 @@ package ilc
 package examples
 package bench
 
+import org.scalameter.api._
+
 class BenchSuite extends BaseBenchmark {
   include[BagUnionBenchmark]
   include[HistogramBenchmark]
   include[MapSuccBenchmark]
   include[SumValuesBenchmark]
+
+  override def reporters = super.reporters :+
+    ChartReporter(ChartFactory.XYLine())
 }

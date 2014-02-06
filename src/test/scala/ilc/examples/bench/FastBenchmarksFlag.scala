2ec3e7998903bf54bd42995a533809d51cf14f6b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-02-06 19:24:03 +0100

    Make benchmark sizes smaller by default (#275)
    @yfcai, that's the idea. Still to test.
    
    Old-commit-hash: f978360f54fbb2afd058f8a95ff51a20bddc31ee

---
+++ src/test/scala/ilc/examples/bench/FastBenchmarksFlag.scala
@@ -0,0 +1,9 @@
+package ilc
+package examples.bench
+
+object FastBenchmarksFlag extends utils.BooleanFlag {
+  /**
+    * Define this to true to speed up benchmarking by reducing the maximum
+    */
+  val value = true
+}

f4649b16335366aa79f3ff5a0854e709d7709853
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-02-10 01:33:38 +0100

    Scala codebase: Finish comment
    Old-commit-hash: 8b0dc8433b8378acdbdfc44094c6760fb92d3301

--- src/test/scala/ilc/examples/bench/FastBenchmarksFlag.scala
+++ src/test/scala/ilc/examples/bench/FastBenchmarksFlag.scala
@@ -4,6 +4,8 @@ package examples.bench
 object FastBenchmarksFlag extends utils.BooleanFlag {
   /**
     * Define this to true to speed up benchmarking by reducing the maximum
+    * dataset size, and the memory allocated by the sub-JVMs set via the -Xmx
+    * option.
     */
   val value = true
 }

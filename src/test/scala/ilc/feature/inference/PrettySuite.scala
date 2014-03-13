167f41c8afb2ec130a24819fa13393fd26701abd
Cai Yufei <cai@mathematik.uni-marburg.de>
2014-03-13 10:57:47 +0100

    test that ambiguities in implicit resolution are caught
    motivation: ccb2ceb9b4dffd8b441798739ef51cf309b8a17b
    
    Old-commit-hash: c584d8c00f3622420a2e8710a5713a92b4d6154c

--- src/test/scala/ilc/feature/inference/PrettySuite.scala
+++ src/test/scala/ilc/feature/inference/PrettySuite.scala
@@ -3,12 +3,53 @@ package ilc.feature.inference
 import org.scalatest._
 import scala.language.implicitConversions
 
+// Clashing* traits add clashing implicit conversions on symbols,
+// for testing that ambiguities are caught at compile time.
+trait ClashingImplicitClass {
+  implicit class ImplicitClass(s: Symbol) { def apply(y: Any) = ??? }
+}
+trait ClashingImplicitConversion {
+  implicit def implicitConversion(s: Symbol) = new ApplyClass
+  class ApplyClass { def apply(y: Any) = ??? }
+}
+
 class PrettySuite
 extends FlatSpec
    with Pretty
    with ilc.feature.maps.Syntax
    with ilc.feature.integers.Syntax
+   with ilc.util.EvalScala
 {
+  "If a 3rd party implicit adding `apply` method to Symbols is in scope," ++
+    " then it " should "fail to compile. Error messages:" in {
+      def errorCode(traitName: String): String =
+        s"""|{ import ilc.feature.inference._
+            |  object X extends Pretty with $traitName { 'x('y) }
+            |}""".stripMargin
+
+      def testTrait(traitName: String, header: String) {
+        val error = intercept[scala.tools.reflect.ToolBoxError] {
+          evalScala(errorCode(traitName))
+        }
+        info(s"$header. ${error.getMessage}")
+      }
+
+      testTrait(
+        "ClashingImplicitClass",
+        "ERROR 1 (clashing implicit class)")
+
+      testTrait(
+        "ClashingImplicitConversion",
+        "ERROR 2 (clashing implicit conversion)")
+
+      testTrait(
+        "org.scalatest.Matchers",
+        "ERROR 3 (ambiguity due to Matchers of scalatest v2.0)")
+
+      // separate error message from other output, enhance readability
+      info("ERRORS OVER\n\n\n")
+    }
+
   "Lambda with symbols and ->:" should "work" in {
     val id: UntypedTerm = 'x ->: 'x
     val id2 = UAbs(UVar("x"), UVar("x"))

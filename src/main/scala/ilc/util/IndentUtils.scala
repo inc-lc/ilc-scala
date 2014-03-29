f1488cca5f5f2c9551409d61c13fd5c95b061a17
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-03-29 20:48:19 +0100

    Make IndentUtils more flexible
    * Allow using indent{Less/More}() in string interpolations
    
    To this end, they must return an empty string instead of (), which would
    alter the output.
    
    * Expose default indentation change
    
    * Allow specifying how much to indent for indentMore()/indentLess()
    
    * Have a method to print a snippet with some extra indentation
    
    Old-commit-hash: a81d2a00683a8d301c794618a2186f51cedfff16

--- src/main/scala/ilc/util/IndentUtils.scala
+++ src/main/scala/ilc/util/IndentUtils.scala
@@ -2,12 +2,20 @@ package ilc
 package util
 
 trait IndentUtils {
-  private var indentDepth: Int = 2
+  protected val indentDiff: Int = 2
+  private var indentDepth: Int = indentDiff
 
   protected def setIndentDepth(i: Int): Unit = { indentDepth = i }
 
-  protected def indentMore() = { indentDepth += 2 }
-  protected def indentLess() = { indentDepth -= 2 }
+  protected def indentMore(delta: Int = indentDiff): String = { indentDepth += delta; "" }
+  protected def indentLess(delta: Int = indentDiff): String = { indentDepth -= delta; "" }
+  protected def deeper(arg: => String, indentDelta: Int = indentDiff): String = {
+    indentMore(indentDelta)
+    val ret = arg
+    indentLess(indentDelta)
+    ret
+  }
+
   protected def indentNoNl(): String = " " * indentDepth
   protected def indent(): String = "\n" + indentNoNl
   protected def openParen(delim: String) = { indentMore(); delim }

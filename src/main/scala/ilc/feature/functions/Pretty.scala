a964927acc4a15ee7a400ccf440722534927c38c
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-03-29 22:24:18 +0100

    Indent for parentheses more robustly and easily
    * The present code does not handle correctly nested parentheses
    * It's also a hack
    
    Instead, simply increase indentation when creating parentheses.
    
    Old-commit-hash: ccc34e29887903a9aebc454eb7b0303cc7d61af4

--- src/main/scala/ilc/feature/functions/Pretty.scala
+++ src/main/scala/ilc/feature/functions/Pretty.scala
@@ -23,7 +23,7 @@ trait Pretty extends Syntax with util.IndentUtils {
     if (bindsStronger(inner, outer))
       text
      else
-       "(" + text + ")"
+       s"(${deeper(text, 1)})"
        //Alternative: have body on separate lines as the parens
        //openParen("(") + indent() + text + closeParen(")")
   }
@@ -62,16 +62,13 @@ trait Pretty extends Syntax with util.IndentUtils {
       //Break lines before the body.
 
       //How much should the body be indented, for perfect visual alignment? That's tricky.
-      val outputStartsWithParen = !bindsStronger(priorityOfAbs, priority)
       val isBodyNestedAbs = body match {
         case Abs(_, _) => true
         case _ => false
       }
       val increaseInnerIndent =
         //Nested abstractions should align with the first one.
-        (if (isBodyNestedAbs) 0 else indentDiff) +
-        //If the output starts with a parenthesis, indent the content by one column.
-        (if (outputStartsWithParen) 1 else 0)
+        (if (isBodyNestedAbs) 0 else indentDiff)
 
       template(priorityOfAbs, priority,
         s"λ${variable.getName.toString}.${deeper(s"$indent${pretty(body, priorityOfAbs + 1)}", increaseInnerIndent)}")

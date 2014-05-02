464a4715150ba8efe79c56d17b084cf51296a482
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-05-02 19:00:16 +0200

    Start adding DBToaster example WIP
    Object-level type inference fails.
    
    Old-commit-hash: 6b30aa7d2f752e887252c48fd79fa1b2eb1a37a9

---
+++ src/main/scala/ilc/examples/DBToasterExamples.scala
@@ -0,0 +1,63 @@
+package ilc
+package examples
+
+import feature._
+
+trait DBToasterExample extends Example
+   with MapReduce
+   with GroupBy
+   with integers.SyntaxSugar
+   with integers.AbelianDerivation
+   with integers.ToScala
+   with equality.Syntax
+   with inference.SyntaxSugar
+ {
+
+}
+
+/**
+ * See Koch et al. (2014), *DBToaster: higher-order delta processing for dynamic, frequently fresh views*.
+ * This is related to example 2.
+ */
+class DBToasterExample2
+extends DBToasterExample
+{
+  def dictFlatMap: UntypedTerm = ???
+  //
+  val dictKeyFilter: UntypedTerm =
+    'valueGroup ->: 'userKeyFilter ->: 'map ->:
+      foldByHom('valueGroup, liftGroup('valueGroup),
+          'k ->: 'v ->: ifThenElse_('userKeyFilter('k), singletonMap('k, 'v), EmptyMap),
+          'map)
+
+  //Produces a map of bags.
+  val bagDictKeyFilter: UntypedTerm = dictKeyFilter(freeAbelianGroup)
+
+  def join: UntypedTerm = ???
+  //XXX better name, eq is taken!
+  val eqq: UntypedTerm = Eq
+
+  //All of this is missing aggregation.
+  def untypedProgram0 = 'r ->: 's ->: join('r, 's, 'rEl ->: 'sEl ->: eqq(proj1('rEl), proj1('sEl)), pair)
+  def untypedProgram =
+    'r ->: groupByGen('r, proj1, proj2)
+    /*'r ->: 's ->: {
+    letS (
+      ('idxR, groupByGen('r, proj1, proj2)),
+      ('idxS, groupByGen('s, proj1, proj2))
+    ) {
+      'idxR
+      //filter (proj1('r) == proj1('s)) ...
+      //dictFlatMap('k ->: 'v ->: bagDictKeyFilter('k2 ->: eqq('k, 'k2), 'idxS))('idxR)
+    }
+    //mapFlatMap apply ('k ->: 'v ->: mapFilter('k2 ->: 'v2 ->: eqq('k, 'k2), groupByGen('s, proj1, proj2))) apply groupByGen('r, proj1, proj2)
+  }*/
+  override val program: Term = 'x ->: 'x //untypedProgram
+
+  def fooMethod() = try {
+    println(pretty(untypedProgram))
+  } catch { case e: inference.Inference#UnificationFailure =>
+    println(e.details)
+  }
+
+}
---
+++ src/main/scala/ilc/feature/inference/SyntaxSugar.scala
@@ -0,0 +1,36 @@
+package ilc
+package feature
+package inference
+
+trait SyntaxSugar extends PrettySyntax with booleans.Syntax {
+  //Basic utils.
+  def asUntyped(t: UntypedTerm) = t
+
+  //"Macros"
+  /** Usage:
+    * {{{
+    * let_x_= {
+    *   stuff
+    * } { x =>
+    *   blah blah x blah
+    * }
+    * }}}
+    */
+  def let(v: Symbol, meaning: UntypedTerm)
+         (body: UntypedTerm): UntypedTerm =
+    (v ->: body)(meaning)
+
+  def letS(pairs: (Symbol, UntypedTerm)*)
+          (body: UntypedTerm): UntypedTerm = {
+    pairs.foldRight(body){ (pair, body) =>
+      let(pair._1, pair._2)(body)
+    }
+  }
+
+  //XXX Should use a macro version of const_.
+  def ifThenElse_(condition: UntypedTerm, thenBranch: UntypedTerm, elseBranch: UntypedTerm) =
+    asUntyped(IfThenElse)(condition, const_(thenBranch), const_(elseBranch))
+
+  //"Libraries"
+  val const_ = asUntyped('a ->: 'b ->: 'a)
+}

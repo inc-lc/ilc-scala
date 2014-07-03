06e535a73cb31896423429183364756aa372ab4a
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-07-03 11:04:39 +0200

    Map-based A-normalization/CSE
    Tillmann suggested this, and it was easy. It seems a small change might
    implement CSE.

--- src/main/scala/ilc/feature/let/ANormalForm.scala
+++ src/main/scala/ilc/feature/let/ANormalForm.scala
@@ -40,3 +40,50 @@ trait ANormalForm extends Syntax with FreeVariables with analysis.Occurrences wi
     }
   }
 }
+
+trait ANormalFormStateful extends Syntax with FreeVariables with analysis.Occurrences with Traversals with ToScala with Pretty with IsAtomic {
+  outer =>
+  private val freshGen = new FreshGen { val syntax: outer.type = outer }
+  import freshGen._
+
+  import collection.mutable
+  //Test how well this works.
+  val doCSE = true
+  def normalizeTerm(t: Term): Term = {
+    val m = mutable.LinkedHashMap.empty[Term, Var]
+    val normalT = normalize(t)(m)
+    m.foldRight(normalT) {
+      case ((term, variable), t) =>
+        Let(variable, term, t)
+    }
+  }
+  def normalize(t: Term)(map: mutable.Map[Term, Var]): Term = t match {
+    case Abs(v, body) =>
+      Abs(v, normalizeTerm(body))
+    case App(operator, operand) =>
+      val s = normalizeName(operator)(map)
+      val t = normalizeName(operand)(map)
+      App(s, t)
+    case Let(variable, exp, body) =>
+      val normalExp = normalizeName(exp, Some(variable))(map)
+      normalize(body)(map)
+    case _ if isAtomic(t) => t
+  }
+
+  def normalizeName(t: Term, boundVar: Option[Var] = None)(map: mutable.Map[Term, Var]) = {
+    val normalT = normalize(t)(map)
+    if (isAtomic(normalT)
+        //Needed, we can't just drop an existing binding.
+        && boundVar == None)
+      normalT
+    else {
+      val newV = boundVar getOrElse fresh("a", normalT.getType)
+      if (doCSE)
+        map getOrElseUpdate (normalT, newV)
+      else {
+        map += (normalT -> newV)
+        newV
+      }
+    }
+  }
+}

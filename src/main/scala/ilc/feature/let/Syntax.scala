ebb4b4c2ea8b2a3d459dc24bf47fa2e5769fb6c6
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-07-04 22:42:05 +0200

    XXX Disable typechecking in Let (to revert)
    Dangerous! But I need this to experiment with static caching, right now
    I have no idea of the needed types.

--- src/main/scala/ilc/feature/let/Syntax.scala
+++ src/main/scala/ilc/feature/let/Syntax.scala
@@ -5,7 +5,7 @@ package let
 trait Syntax extends functions.Syntax {
   case class Let(variable: Var, exp: Term, body: Term) extends Term {
     override lazy val getType = {
-      assert (variable.getType == exp.getType)
+      //assert (variable.getType == exp.getType)
       body.getType
     }
   }

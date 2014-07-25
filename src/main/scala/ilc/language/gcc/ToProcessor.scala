2080fefb2be85acbf800c1c14321b0ccab50c949
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-07-25 20:08:23 +0200

    GCC compiler WIP
    Comments detail what's left to do.
    
    Old-commit-hash: e4417e1fa96e4afad0f7ad9b0aaf1f2f04fb443e

--- src/main/scala/ilc/language/gcc/ToProcessor.scala
+++ src/main/scala/ilc/language/gcc/ToProcessor.scala
@@ -11,6 +11,10 @@ trait BasicDefinitions {
 
   trait Instr
   type Block = List[Instr]
+
+  //case class Frame(v: Var)
+  case class Frame(vars: List[Var])
+  val emptyFrame = Frame(Nil)
 }
 
 trait TopLevel {
@@ -19,8 +23,8 @@ trait TopLevel {
   // The top-level is special!
   // It needs the
 
-  case class TopLevel(names: List[Var], blocks: List[Block])
-  def emptyTopLevel = TopLevel(Nil, Nil)
+  //case class TopLevel(names: List[Var], blocks: List[Block])
+  //def emptyTopLevel = TopLevel(Nil, Nil)
   //case class Program(t: TopLevel)
 
   def checkTopConsistent(): Unit =
@@ -37,12 +41,13 @@ trait TopLevel {
     topBlocks += b
     checkTopConsistent()
   }
+
+  def topFrame(): Frame = Frame(topNames.toList)
 }
 
 trait Instructions {
   outer: Syntax with BasicDefinitions =>
 
-  case class LDF() extends Instr
   case class AP(n: Int) extends Instr
   case class DeBrujinIdx
     ( n: Int //Number of binders to go down
@@ -58,9 +63,9 @@ trait ToProcessor extends BasicDefinitions with TopLevel with Instructions {
   val freshener = new FreshGen {
     val syntax: outer.type = outer
   }
-
-  //case class Frame(v: Var)
-  case class Frame(vars: List[Var])
+  case class LDF(n: Var) extends Instr {
+    assert(topNames contains n)
+  }
 
   def varInFrame(v: Var)(l: Frame): Option[Int] = {
     val idx = l.vars indexOf v
@@ -71,28 +76,44 @@ trait ToProcessor extends BasicDefinitions with TopLevel with Instructions {
     //if (v == l.v) Some(0) else None
   }
 
-  def toIdx(v: Var, l: List[Frame]): DeBrujinIdx = {
-    l.map(varInFrame(v)).zipWithIndex.collect {
+  def toIdx(v: Var, frames: List[Frame]): DeBrujinIdx = {
+    val adjFrames = frames ++ List(topFrame())
+    val results = adjFrames.map(varInFrame(v)).zipWithIndex.collect {
       case (Some(idx), idx2) =>
         DeBrujinIdx(idx, idx2, v)
-    }.head
+    }
+    assert (results.length == 1) //We don't expect shadowing, do we?
+    results.head //Finds the left-most (that is, innermost) binding (in case we do support it)
   }
 
-  def toProc(t: Term, vars: List[Frame]): List[Instr] = t match {
+  def toProc(t: Term, frames: List[Frame]): Block = t match {
+    //XXX: - Add more cases
+    //XXX: - Add Let, and especially LetRecs. We need to support a LetRec* at the top-level, but probably we can just as well support it everywhere
+    //by collecting the definitions, so that we don't need to use mutation for the top-level. (They don't really do that either).
+    //XXX: - test this!!!
     case App(f, arg) =>
-      toProc(f, vars) ++ toProc(arg, vars) ++ List(AP(1))
-    case Abs(variable, body) =>
-      val v: Var = freshener.fresh("fun", variable.getType =>: body.getType)
-      //this.add
+      val arity = 1 //XXX generalize!
+      toProc(f, frames) ++ toProc(arg, frames) ++ List(AP(arity))
 
-      //val newTop = TopLevel(???)
-      //Allow recursion
-      val compiledBody = toProc(body, Frame(List(variable)) :: vars)
+    //Maybe the current "top-level" handling should instead be used just for letrec*?
+    case Abs(variable, body) =>
+      //We have to lift the body to the top.
+      //But we don't do lambda-lifting because we still expect to find the
+      val v: Var = freshener.fresh("fun", variable.getType =>: body.getType)
+      //"Allow recursion by adding the name before compiling the body." XXX
+      // Doesn't make sense, because the name is not part of the program.
+      // That would make sense for letrec* though.
+      addTopLevelName(v)
+      val compiledBody = toProc(body, Frame(List(variable)) :: frames)
       //Add compiledBody with a fresh name to the environment.
-      val idx = ???
-      List(LD(idx))
+      addTopLevelBlock(compiledBody)
+      //XXX That's for accessing globals, not for creating closures.
+      //val idx = toIdx(v, frames)
+      //List(LD(idx))
+      //Create the closure here.
+      List(LDF(v))
     case v @ Var(name, _) =>
-      List(LD(toIdx(v, vars)))
+      List(LD(toIdx(v, frames)))
     case _ =>
       ???
   }

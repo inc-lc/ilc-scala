015034cfa73a4abb0b5b6f859b43e664f539fc65
Jonathan <jonathan@b-studios.de>
2014-07-27 00:36:38 +0200

    Forgot to add files
    Old-commit-hash: 8e257523988493d128c7a4a41fd5c958b0ab669e

---
+++ src/main/scala/ilc/feature/lists/Types.scala
@@ -0,0 +1,9 @@
+package ilc
+package feature
+package lists
+
+import ilc.util.ExtractorTrait
+
+trait Types extends base.Types with ExtractorTrait {
+  case class ListType(elemType: Type) extends Type
+}

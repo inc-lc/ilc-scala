e2848b7ae765fd50e645181d186b846f4abf53e2
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-09-05 22:28:11 +0200

    Comments on Stability

--- src/main/scala/ilc/analysis/Stability.scala
+++ src/main/scala/ilc/analysis/Stability.scala
@@ -3,11 +3,26 @@ package analysis // package of static analyses
 
 import ilc.feature.functions
 
+/**
+  * Assume you're interested in deriving a closed function f.
+  * You want to know how the result changes if the input arguments of f change.
+  * By extension, you want to know how subterms of f change when the inputs of f change.
+  * Some subterms will never change. We say these subterms are *stable*.
+  * Stability analysis figures that out.
+  *
+  * @author Cai Yufei
+  * @author Paolo Giarrusso for extra documentation (adapted from Cai's stability.md)
+  */
 trait Stability
 extends functions.Context
    with FreeVariables
 {
-
+  /**
+    * This represents the internal state of stability analysis — its context.
+    * To compute the stability of a term, we are interested in knowing:
+    * - whether its arguments are stable.
+    * - which variables are stable.
+    */
   case class Stability(
     ofVariables: Map[Var, Boolean],
     ofArguments: Stream[Boolean]
@@ -15,6 +30,14 @@ extends functions.Context
 
 
   object Stability {
+    /**
+      * This is the stability of the program f. This value asserts that we know
+      * nothing about the stability of arguments, and that all arguments are
+      * unstable.
+      *
+      * This does not mean that f itself is unstable. In fact, f itself is
+      * always stable, and its body might be stable if f is a constant function.
+      */
     def entirelyUnstable: Stability =
       Stability(Map.empty.withDefaultValue(false), unknownArguments)
 
@@ -49,12 +72,14 @@ extends functions.Context
                             pArgStability)
 
               case 1 =>
+                //For arguments, we punt, and just say that we don't know if their arguments are stable or not.
                 Stability(pVarStability, Stability.unknownArguments)
             }
           }
         }
       }
 
+    // A term is stable if all its free variables are stable.
     def isStableGiven(pVarStability: Map[Var, Boolean]): Boolean =
       (subterm.freeVariables map pVarStability).fold(true)(_ && _)
   }

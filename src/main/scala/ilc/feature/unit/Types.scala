ac3ed4eb1130b4b85064e91c93dca3e1d36457a3
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-09-24 23:25:32 +0200

    Better (Unicode) pretty-printing for Unit type

--- src/main/scala/ilc/feature/unit/Types.scala
+++ src/main/scala/ilc/feature/unit/Types.scala
@@ -3,5 +3,7 @@ package feature
 package unit
 
 trait Types extends base.Types {
-  case object UnitType extends Type
+  case object UnitType extends Type {
+    override def toString = UnicodeOutput.choose("𝟙", "1")
+  }
 }

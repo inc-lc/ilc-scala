ced63f1ec55b5b3ad4287243ee1b8e220d252f0a
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-12-23 23:54:30 +0100

    Add Plotkin's CBV CPS transformation

---
+++ src/main/scala/ilc/feature/let/CPS.scala
@@ -0,0 +1,30 @@
+package ilc
+package feature
+package let
+
+object CPS extends functions.Syntax {
+  outer =>
+
+  //An implementation of Plotkin's CBV CPS transformation.
+  case object AnswerT extends Type
+  def toCPST(t: Type) = (t =>: AnswerT) =>: AnswerT
+  def kVar(t: Term): Var = Var("k", toCPST(t.getType))
+
+  def toCPS: Term => Term = {
+    case t @ App(f, arg) =>
+      lambda(kVar(t))(k =>
+        toCPS(f) ! lambda(Var("a", f.getType))(a =>
+          toCPS(arg) ! lambda(Var("b", arg.getType))(b =>
+            a ! b ! k)))
+    /*
+     * Plotkin's paper has three separate identical cases. However, they are
+     * the same, and once one looks at how one does a CPS transformation by going
+     * to CBPV first, that becomes obvious: computations need to be
+     * sequentialized, while values simply need to be turned into trivial
+     * computations.
+     */
+    case value =>
+      val k = kVar(value)
+      Abs(k, App(k, value))
+  }
+}

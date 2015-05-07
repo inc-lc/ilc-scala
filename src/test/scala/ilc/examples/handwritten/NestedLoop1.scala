ee04ebaaf54e99b0171a13dd8248918a642c11b8
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-05-07 16:44:01 +0200

    Fixup

--- src/test/scala/ilc/examples/handwritten/NestedLoop1.scala
+++ src/test/scala/ilc/examples/handwritten/NestedLoop1.scala
@@ -3,26 +3,27 @@ package examples
 package handwritten
 
 import feature.bags.Library._
+import org.scalameter.api._
 
-class NestedLoop1 {
-  val N = 0
+class NestedLoop1 extends Serializable {
+  val N = 10
   val coll1Init = List[Int](0 to N - 1: _*)
   val coll2Init = List[Int](1 to N: _*)
 
   //On lists first. Bag[T] is a Map[T, Int], so it's less clear how to write this on maps.
-  def nestedLoop1(coll1: List[Int], coll2: List[Int]) : List[Int] =
+  def nestedLoop1(coll1: List[Int], coll2: List[Int]): List[Int] =
     for {
       i <- coll1
       j <- coll2
     } yield (i * N + j)
 
-  def nestedLoop2(coll1: List[Int], coll2: List[Int]) : List[Int] = {
+  def nestedLoop2(coll1: List[Int], coll2: List[Int]): List[Int] = {
     val g = (i: Int) => (j: Int) => i * N + j
     val f = (i: Int) => coll2.map(g(i))
     coll1.flatMap(f)
   }
 
-  def nestedLoop3(coll1: List[Int], coll2: List[Int]) : List[Int] = {
+  def nestedLoop3(coll1: List[Int], coll2: List[Int]): List[Int] = {
     val g = (i: Int) => (j: Int) => i * N + j
     val f = (i: Int) => coll2.map(g(i))
     coll1.map(f).flatten
@@ -34,6 +35,8 @@ class NestedLoop1Bench extends BaseBenchmark {
   import n._
 
   performance of "nestedLoop1" in {
-    nestedLoop1(coll1Init, coll2Init)
+    using(Gen.unit("dummy")) config testConfig in { _ =>
+      nestedLoop1(coll1Init, coll2Init)
+    }
   }
 }

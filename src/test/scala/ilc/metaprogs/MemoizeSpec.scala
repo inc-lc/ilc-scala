9b67bed1738aa8dd16800faebdc556381cfcf4ab
Yufei Cai <yufei.cai@uni-tuebingen.de>
2015-05-28 10:23:59 +0200

    update memoizedDerive correctness test on integer addition
    -    //XXX: This kind of code will not produce the desired effect.
    -    //One needs to first run the transformed base program and then memoizedDerive, in a shared context (where the caches are bound), to have the cache contain values.
    -    //Code along the lines below should also give the correct result, but that's not expected to be fast.
    
    The old test did not run original program and memoized derivative
    with the same caches. It didn't take advantage of memoization
    performance-wise, and it didn't test cache lookup.

--- src/test/scala/ilc/metaprogs/MemoizeSpec.scala
+++ src/test/scala/ilc/metaprogs/MemoizeSpec.scala
@@ -54,24 +54,69 @@ class MemoizeSpec extends FlatSpec with Memoize
     evalScala(source).asInstanceOf[T]
   }
 
+  // compile programs in a shared context,
+  // return right-biased nested tuples
+  def compileSeq[T](t: Term, ts: Term*): T =
+    compile[T](toNestedPairs(t, ts: _*))
+
+  def toNestedPairs(t: Term, ts: Term*): Term =
+    if (ts.isEmpty)
+      t
+    else
+      Pair ! t ! toNestedPairs(ts.head, ts.tail: _*)
+
   it should "produce code that compiles to well-typed Scala" in {
     val t = asTerm(('x % IntType) ->: ('y % IntType) ->: (PlusInt('x, 'y)))
-    compile[Any](transform(t))
-    compile[Any](memoizedDerive(t))
+    compileSeq[Any](transform(t), memoizedDerive(t))
   }
 
   it should "produce correct results for multivariate functions" in {
     import abelianGroups.Library._
 
-    //XXX: This kind of code will not produce the desired effect.
-    //One needs to first run the transformed base program and then memoizedDerive, in a shared context (where the caches are bound), to have the cache contain values.
-    //Code along the lines below should also give the correct result, but that's not expected to be fast.
-    type DInt = ((=> Int) => Either[(AbelianGroup[Int], Int), Int])
-    val t  = asTerm(('x % IntType) ->: ('y % IntType) ->: (PlusInt('x, 'y)))
-    val dt = memoizedDerive(t)
-    val f  = compile[(=> Int) => (=> Int) => Int](t)
-    val df = compile[(=> Int) => DInt => (=> Int) => DInt => Int](dt)
-    assert(f(1)(2) == 3)
-    // TODO: assert derivative produce correct change when given changing x and unchanging y
+    type DInt = Either[(AbelianGroup[Int], Int), Int]
+
+    val t0 = asTerm(('x % IntType) ->: ('y % IntType) ->: (PlusInt('x, 'y)))
+    val t  = transform(t0)
+    val dt = memoizedDerive(t0)
+
+    // run t to compute 1 + 2 & save results in the cache
+    val x0   = 1
+    val y0   = 2
+    val run0 = asTerm(t ! x0 ! y0)
+    val val0 = 3
+
+    // build a term that runs dt with replacement changes
+    def runWithChange(newX: Int, newY: Int): Term =
+      (updateTerm(IntType) !
+        (dt ! x0 ! (replacementChange ! newX) ! y0 ! (replacementChange ! newY)) !
+        val0)
+
+
+    val (delta1, delta2) = (10, 100)
+
+    // run dt with x and y unchanged
+    val run1 = runWithChange(x0, y0)
+    val val1 = val0
+
+    // run dt with x changed and y unchanged
+    val run2 = runWithChange(x0 + delta1, y0)
+    val val2 = val0 + delta1
+
+    // run dt with x unchanged and y changed
+    val run3 = runWithChange(x0, y0 + delta2)
+    val val3 = val0 + delta2
+
+    // run dt with x and y changed
+    val run4 = runWithChange(x0 + delta1, y0 + delta2)
+    val val4 = val0 + delta1 + delta2
+
+    val (res0, (res1, (res2, (res3, res4)))) =
+      compileSeq[(Int, (Int, (Int, (Int, Int))))](run0, run1, run2, run3, run4)
+
+    assert(res0 == val0)
+    assert(res1 == val1)
+    assert(res2 == val2)
+    assert(res3 == val3)
+    assert(res4 == val4)
   }
 }

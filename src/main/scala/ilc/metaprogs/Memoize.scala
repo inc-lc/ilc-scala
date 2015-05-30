86f58dfa96c0c10701a97193ea60d3158e2006b0
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-05-31 01:56:39 +0200

    Fixup code indent

--- src/main/scala/ilc/metaprogs/Memoize.scala
+++ src/main/scala/ilc/metaprogs/Memoize.scala
@@ -14,61 +14,61 @@ trait Memoize extends memoize.MemoizeBase {
   outer: ilc.feature.functions.Syntax with let.Syntax with base.Derivation with memoize.Syntax with analysis.FreeVariables with base.ToScala =>
 
   class MemoContext(cacheMap: mutable.Map[Term, CacheEntry] = mutable.Map[Term, CacheEntry]()) extends MemoContextBase(cacheMap) {
-  def memoizedDerive(t: Term): Term = t match {
-    case Abs(x, body) =>
-      lambdaTerm(x, DVar(x)) { memoizedDerive(body) }
+    def memoizedDerive(t: Term): Term = t match {
+      case Abs(x, body) =>
+        lambdaTerm(x, DVar(x)) { memoizedDerive(body) }
 
-    case App(operator, operand) =>
-      //XXX: What happens here if we have two instances of the same term around?
-      val memoizedOperand: Term = Memo(cacheMap(operand), updateCache = false) ! operand
-      memoizedDerive(operator) !
-        //operand ! // In non-memoizing derivation
-        memoizedOperand ! // Main (only) change from non-memoizing derivation!
-        memoizedDerive(operand)
+      case App(operator, operand) =>
+        //XXX: What happens here if we have two instances of the same term around?
+        val memoizedOperand: Term = Memo(cacheMap(operand), updateCache = false) ! operand
+        memoizedDerive(operator) !
+          //operand ! // In non-memoizing derivation
+          memoizedOperand ! // Main (only) change from non-memoizing derivation!
+          memoizedDerive(operand)
 
-    case v: Var =>
-      DVar(v)
+      case v: Var =>
+        DVar(v)
 
-    // For all terms we don't know how to derive,
-    // we produce a derivative that does recomputation.
-    // This makes adding new constants easy.
-    case _ =>
-      Diff ! t ! t
-  }
-
-  def doTransform(t: Term, freeVars: List[Var], m: Map[Term, Name] = Map.empty): Term = {
-    val cacheEntry = getOrElseNewCacheEntry(t)
-
-    /*
-    val memoizedSubterms: Term = t match {
-      case App(s, t) => App(doTransform(s, freeVars), doTransform(t, freeVars))
-      case Abs(x, t) => Abs(x, doTransform(t, x :: freeVars))
-      //case x: Var => x
-      case x => x
+      // For all terms we don't know how to derive,
+      // we produce a derivative that does recomputation.
+      // This makes adding new constants easy.
+      case _ =>
+        Diff ! t ! t
     }
 
-    Memo(cacheEntry) ! memoizedSubterms
-    */
-    def memoNode = Memo(cacheEntry, updateCache = true)
-    t match {
-      case Let(x, term, body) =>
-        memoNode !
-          Let(x, doTransform(term, freeVars), doTransform(body, freeVars))
-      case App(s, t) =>
-        memoNode !
-          App(doTransform(s, freeVars), doTransform(t, freeVars))
-      case Abs(x, t) =>
-        //XXX This will memoize each function in a chain of nested lambdas.
-        // But this can only be solved through CBPV.
-        memoNode !
-          Abs(x, doTransform(t, x :: freeVars))
-      //case x: Var => x
-      //Otherwise, for atoms (variables and constants), do *no* memoization.
-      case x => x
+    def doTransform(t: Term, freeVars: List[Var], m: Map[Term, Name] = Map.empty): Term = {
+      val cacheEntry = getOrElseNewCacheEntry(t)
+
+      /*
+      val memoizedSubterms: Term = t match {
+        case App(s, t) => App(doTransform(s, freeVars), doTransform(t, freeVars))
+        case Abs(x, t) => Abs(x, doTransform(t, x :: freeVars))
+        //case x: Var => x
+        case x => x
+      }
+
+      Memo(cacheEntry) ! memoizedSubterms
+      */
+      def memoNode = Memo(cacheEntry, updateCache = true)
+      t match {
+        case Let(x, term, body) =>
+          memoNode !
+            Let(x, doTransform(term, freeVars), doTransform(body, freeVars))
+        case App(s, t) =>
+          memoNode !
+            App(doTransform(s, freeVars), doTransform(t, freeVars))
+        case Abs(x, t) =>
+          //XXX This will memoize each function in a chain of nested lambdas.
+          // But this can only be solved through CBPV.
+          memoNode !
+            Abs(x, doTransform(t, x :: freeVars))
+        //case x: Var => x
+        //Otherwise, for atoms (variables and constants), do *no* memoization.
+        case x => x
+      }
+
     }
 
-  }
-
-  def transform(t: Term) = doTransform(t, List())
+    def transform(t: Term) = doTransform(t, List())
   }
 }

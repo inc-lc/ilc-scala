4a5c1599f962166fa27a0f0dd00bc3deb93f8900
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2016-01-28 21:26:45 +0100

    Implement freeVars on UntypedSyntax
    This will be needed next for refactoring type inference.

--- src/main/scala/ilc/feature/inference/UntypedSyntax.scala
+++ src/main/scala/ilc/feature/inference/UntypedSyntax.scala
@@ -13,16 +13,39 @@ trait UntypedSyntax {
   case class UMonomorphicConstant(term: Term) extends UntypedTerm
   case class UPolymorphicConstant(term: PolymorphicConstant) extends UntypedTerm
   case class TypeAscription(term: UntypedTerm, typ: Type) extends UntypedTerm
+
+  def freeVars(t: UntypedTerm): Set[Name] = t match {
+    case UVar(n)                 => Set(n)
+    case UAbs(n, _, body)        => freeVars(body) - n
+    case UApp(f, arg)            => freeVars(f) ++ freeVars(arg)
+    case TypeAscription(t, _)    => freeVars(t)
+    case UMonomorphicConstant(_) => Set.empty
+    case UPolymorphicConstant(_) => Set.empty
+  }
 }
 
 trait LetUntypedSyntax extends UntypedSyntax {
   this: base.Syntax with functions.Syntax =>
 
   case class ULet(variable: Name, exp: UntypedTerm, body: UntypedTerm) extends UntypedTerm
+  override def freeVars(t: UntypedTerm) = t match {
+    case ULet(v, exp, body) =>
+      // Parens are important here: if there's shadowing, v can be free on the
+      // LHS.
+      freeVars(exp) ++ (freeVars(body) - v)
+    case _ =>
+      super.freeVars(t)
+  }
 }
 
 trait LetRecUntypedSyntax extends UntypedSyntax {
   this: base.Syntax with functions.Syntax =>
 
   case class ULetRec(pairs: List[(Name, UntypedTerm)], bodyName: Name, body: UntypedTerm) extends UntypedTerm
+  override def freeVars(t: UntypedTerm) = t match {
+    case ULetRec(pairs, _, body) =>
+      freeVars(body) ++ (pairs flatMap (x => freeVars(x._2))) -- (pairs map (_._1))
+    case _ =>
+      super.freeVars(t)
+  }
 }

19faa27b9238a5c79cd8ab4ec4cb6991ce626290
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2016-02-01 02:18:13 +0100

    Add `getType`-like methods for CBPV

--- src/main/scala/ilc/feature/cbpv/Syntax.scala
+++ src/main/scala/ilc/feature/cbpv/Syntax.scala
@@ -26,17 +26,25 @@ trait Syntax extends functions.Syntax with Types with TypeUtils {
 //XXX the naming scheme is broken: VT and CT are also used as suffixes for value
 //types.
 trait CBPVSyntax extends CBPVTypes with base.Names {
-  trait CompTerm
-  trait ValTerm
+  sealed trait CompTerm {
+    def compType: CompType = null
+  }
+  sealed trait ValTerm {
+    def valType: ValType = null
+  }
 
   /////////////////////
   //   Value terms   //
   /////////////////////
 
   //vVar : ∀ {τ} (x : ValVar Γ τ) → Val Γ τ
-  case class VarVT(n: Name, t: ValType) extends ValTerm
+  case class VarVT(n: Name, t: ValType) extends ValTerm {
+    override def valType = t
+  }
   //vThunk : ∀ {τ} → Comp Γ τ → Val Γ (U τ)
-  case class ThunkVT(c: CompTerm) extends ValTerm
+  case class ThunkVT(c: CompTerm) extends ValTerm {
+    override lazy val valType = UThunkVT(c.compType)
+  }
 
   // And value constants:
   //vConst : ∀ {Σ τ} →
@@ -51,25 +59,52 @@ trait CBPVSyntax extends CBPVTypes with base.Names {
   //cAbs : ∀ {σ τ} →
   //  (t : Comp (σ •• Γ) τ) →
   //  Comp Γ (σ ⇛ τ)
-  case class AbsCT(n: VarVT, body: CompTerm) extends CompTerm
+  case class AbsCT(n: VarVT, body: CompTerm) extends CompTerm {
+    override lazy val compType = FunCT(n.valType, body.compType)
+  }
 
   //cApp : ∀ {σ τ} →
   //  (s : Comp Γ (σ ⇛ τ)) →
   //  (t : Val Γ σ) →
   //  Comp Γ τ
-  case class AppCT(f: CompTerm, arg: ValTerm) extends CompTerm
+  case class AppCT(f: CompTerm, arg: ValTerm) extends CompTerm {
+    override lazy val compType = {
+      f.compType match {
+        case FunCT(s, t) if s == arg.valType =>
+          t
+        case _ =>
+          ???
+      }
+    }
+  }
 
   //_into_ : ∀ {σ τ} →
   //  (e₁ : Comp Γ (F σ)) →
   //  (e₂ : Comp (σ •• Γ) (F τ)) →
   //  Comp Γ (F τ)
-  case class BindCT(e1: CompTerm, n: VarVT, e2: CompTerm) extends CompTerm
+  case class BindCT(e1: CompTerm, n: VarVT, e2: CompTerm) extends CompTerm {
+    override lazy val compType = {
+      if (e1.compType == FProducerCT(n.valType))
+        e2.compType
+      else
+        ???
+    }
+  }
 
   //cForce : ∀ {τ} → Val Γ (U τ) → Comp Γ τ
-  case class ForceCT(v: ValTerm) extends CompTerm
+  case class ForceCT(v: ValTerm) extends CompTerm {
+    override lazy val compType = {
+      v.valType match {
+        case UThunkVT(ct) => ct
+        case _ => ???
+      }
+    }
+  }
 
   //cReturn : ∀ {τ} (v : Val Γ τ) → Comp Γ (F τ)
-  case class ReturnCT(v: ValTerm) extends CompTerm
+  case class ReturnCT(v: ValTerm) extends CompTerm {
+    override lazy val compType = FProducerCT(v.valType)
+  }
 
   // And computation constants:
   //cConst : ∀ {Σ τ} →

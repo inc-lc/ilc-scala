28978f73573f62e9f749f0dcc1fc5413a69b10ae
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2016-02-01 13:28:31 +0100

    Sketch NbE for CBPV

--- src/main/scala/ilc/feature/cbpv/Syntax.scala
+++ src/main/scala/ilc/feature/cbpv/Syntax.scala
@@ -115,7 +115,7 @@ trait CBPVSyntax extends CBPVTypes with base.Names {
 
 trait SyntaxConversions extends functions.Syntax with CBPVSyntax with TypeConversions {
   outer =>
-  private val freshGen = new base.FreshGen { lazy val syntax: outer.type = outer }
+  protected[cbpv] val freshGen = new base.FreshGen { lazy val syntax: outer.type = outer }
   import freshGen.freshName
 
   def cbnToCBPV(t: Term): CompTerm = t match {
@@ -144,5 +144,144 @@ trait SyntaxConversions extends functions.Syntax with CBPVSyntax with TypeConver
           AppCT(ForceCT(fV), argV)))
     case value =>
       ReturnCT(cbvValueToCBPV(value))
+  }
+}
+
+trait CBPVNbE extends SyntaxConversions {
+  import freshGen.freshName
+  /*
+  //Attempt 1:
+  def cbpvToCbpvForTypeVal(vt: ValTerm, vTyp: ValType): ValTerm = {
+    vt
+  }
+  */
+
+  /*
+  def cbpvToCbpvForTypeComp(ct: CompTerm, cTyp: CompType): CompTerm = {
+    cTyp match {
+      case FProducerCT(UThunkVT(cTypInner)) =>
+        ReturnCT(ThunkVT(cbpvToCbpvForTypeComp(ct, cTypInner)))
+      case FProducerCT(vTyp) =>
+        //XXX, no, insert a return and a bind. Optimizing:
+        ct match {
+          case ReturnCT(vt) =>
+            ReturnCT(cbpvToCbpvForTypeVal(vt, vTyp))
+          case AbsCT(_, _) =>
+            sys error "impossible"
+          case AppCT(funCT, argVT) =>
+            ???
+          case BindCT(e1, n, e2) =>
+            ???
+          case ForceCT(vt) =>
+            //cTyp = F vTyp
+            //ct = force vt
+            //vt: U (ct.compType)
+            ForceCT(cbpvToCbpvForTypeVal(vt, UThunkVT(cTyp)))
+        }
+      case FunCT(s, t) =>
+        ct match {
+          case AbsCT(v, body) =>
+            AbsCT(v, cbpvToCbpvForTypeComp(body, t))
+          case _ =>
+            val v = VarVT(freshName("eta"), s)
+            // Do I need to convert `ct` again here?
+            AbsCT(v, cbpvToCbpvForTypeComp(AppCT(ct /* ??? */ , v), t))
+        }
+      case ProdCT(a, b) =>
+        ???
+    }
+    // Too hard maybe. But the idea was to do this in a type-directed way — matching the actual and expected type!
+  }
+  */
+
+  /*
+  def cbpvToCbpvForTypeComp(ct: CompTerm, cTyp: CompType): CompTerm = {
+    // Too easy — we must match structurally the types, because the mismatch might occur in a nested position.
+    if (cTyp == ct.compType)
+      ct
+    else if (cTyp == FProducerCT(UThunkVT(ct.compType)))
+      ReturnCT(ThunkVT(cbpvToCbpvForTypeComp(ct, ct.compType)))
+    else if (ct.compType == FProducerCT(UThunkVT(cTyp))) {
+      val freshVar = VarVT(freshName("eta"), UThunkVT(cTyp))
+      BindCT(ct, freshVar, ForceCT(freshVar))
+    } else {
+      ???
+      //sys error "Hopefully impossible"
+    }
+  }
+  */
+
+  sealed trait Value
+  case class Thunk(cv: CompValue) extends Value
+  case class ResidualVar(v: VarVT) extends Value
+
+  sealed trait CompValue
+  case class FunVal(v: VarVT, fun: Value => CompValue) extends CompValue
+  case class Return(v: Value) extends CompValue
+  sealed trait NeutralCompValue extends CompValue
+  case class CompTermVal(ct: CompTerm) extends NeutralCompValue
+  case class ResidualApp(funCT: NeutralCompValue, argVT: Value) extends NeutralCompValue
+  case class ResidualBind(cv1: NeutralCompValue, v: VarVT, cv2Fun: Value => CompValue) extends NeutralCompValue
+  case class ResidualForce(vt: Value) extends NeutralCompValue
+
+  def eval(vt: ValTerm, env: Map[Name, Value]): Value = {
+    vt match {
+      case VarVT(n, _) => env(n)
+      case ThunkVT(ct) =>
+        Thunk(compute(ct, env))
+    }
+  }
+
+  def compute(ct: CompTerm, env: Map[Name, Value]): CompValue = {
+    ct match {
+      case AbsCT(v, body) =>
+        FunVal(v, arg => compute(body, env + (v.n -> arg)))
+      case AppCT(fun, arg) =>
+        val fCV = compute(fun, env)
+        val argV = eval(arg, env)
+        fCV match {
+          case FunVal(v, funScala) =>
+            funScala(argV)
+          case ncv: NeutralCompValue =>
+            ResidualApp(ncv, argV)
+          case Return(_) =>
+            sys error "type error"
+        }
+      case BindCT(ct1, v, ct2) =>
+        val cv1 = compute(ct1, env)
+        cv1 match {
+          case Return(v1) =>
+            compute(ct2, env + (v.n -> v1))
+          case ncv: NeutralCompValue =>
+            ResidualBind(ncv, v, arg => compute(ct2, env + (v.n -> arg)))
+          case FunVal(v, funScala) =>
+            sys error "type error"
+        }
+      case ReturnCT(vt) =>
+        val v = eval(vt, env)
+        Return(v)
+      case ForceCT(vt) =>
+        val v = eval(vt, env)
+        v match {
+          case Thunk(c) =>
+            c
+          case _ =>
+            ResidualForce(v)
+        }
+    }
+  }
+
+  def reifyVal(v: Value): ValTerm = v match {
+    case Thunk(cv) => ThunkVT(reifyComp(cv))
+    case ResidualVar(v) => v
+  }
+
+  def reifyComp(cv: CompValue): CompTerm = cv match {
+    case FunVal(v, fun) => AbsCT(v, reifyComp(fun(ResidualVar(v))))
+    case Return(v) => ReturnCT(reifyVal(v))
+    case ResidualApp(funCT, argVT) => AppCT(reifyComp(funCT), reifyVal(argVT))
+    case ResidualBind(cv1, v, cv2Fun) => BindCT(reifyComp(cv1), v, reifyComp(cv2Fun(ResidualVar(v))))
+    case ResidualForce(v) => ForceCT(reifyVal(v))
+    case CompTermVal(ct) => ct
   }
 }
